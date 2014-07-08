cf344d7de37d34c53aac6976f5c3f7d9906e52b0
Julian Krumow <julian.krumow@artcom.de>
2014-07-08 12:00:22 +0200

    - removed buildbreaker

--- jsonrpc-common/src/main/java/better/jsonrpc/core/JsonRpcExecutorConnection.java
+++ jsonrpc-common/src/main/java/better/jsonrpc/core/JsonRpcExecutorConnection.java
@@ -1,6 +1,4 @@
 package better.jsonrpc.core;
-
-import better.jsonrpc.util.RunnableHandlingExceptions;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
