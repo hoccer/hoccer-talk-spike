70fe846768c7f492457e47018cff689dc7699097
jacob <jacob.zschunke@artcom.de>
2014-07-14 16:29:19 +0200

    moved intent from XoPreferenceActivity to the preference xml

--- android/src/main/java/com/hoccer/xo/android/activity/XoPreferenceActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/XoPreferenceActivity.java
@@ -159,15 +159,7 @@ public class XoPreferenceActivity extends PreferenceActivity
         } else if (preference.getKey().equals("preference_import")) {
             doImport();
             return true;
-        } else if (preference.getKey().equals("preference_about")) {
-            showAbout();
-        } else if (preference.getKey().equals("preference_licenses")) {
-            showLicense();
-        } else if (preference.getKey().equals("preference_faq")) {
-            showFaq();
-        } else if (preference.getKey().equals("preference_tutorial")) {
-            showTutorial();
-        }
+        } 
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
@@ -262,30 +254,6 @@ public class XoPreferenceActivity extends PreferenceActivity
             Toast.makeText(this, R.string.export_credentials_failure, Toast.LENGTH_LONG).show();
         }
         Toast.makeText(this, R.string.export_credentials_success, Toast.LENGTH_LONG).show();
-    }
-
-    private void showAbout() {
-        Intent intent = new Intent(this, LegalImprintActivity.class);
-        intent.putExtra(LegalImprintActivity.DISPLAY_MODE, LegalImprintActivity.SHOW_ABOUT);
-        startActivity(intent);
-    }
-
-    private void showLicense() {
-        Intent intent = new Intent(this, LegalImprintActivity.class);
-        intent.putExtra(LegalImprintActivity.DISPLAY_MODE, LegalImprintActivity.SHOW_LICENSES);
-        startActivity(intent);
-    }
-
-    private void showTutorial() {
-        Intent intent = new Intent(this, FaqTutorialActivity.class);
-        intent.putExtra("URL", getResources().getString(R.string.link_tutorial));
-        startActivity(intent);
-    }
-
-    private void showFaq() {
-        Intent intent = new Intent(this, FaqTutorialActivity.class);
-        intent.putExtra("URL", getResources().getString(R.string.link_faq));
-        startActivity(intent);
     }
 
 }
