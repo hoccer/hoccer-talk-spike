374ad536112e258ea70468268e05fb7b6574c4a1
Julian Krumow <julian.krumow@artcom.de>
2014-07-21 10:56:10 +0200

    Adds support for nicknames in action bar. Fixes #225

--- android/src/main/java/com/hoccer/xo/android/activity/MessagingActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/MessagingActivity.java
@@ -176,10 +176,14 @@ public class MessagingActivity extends XoActivity implements IXoContactListener 
 
     public void setActionBarText(TalkClientContact contact) {
         String title;
-        if (contact.isGroup() && contact.getGroupPresence().isTypeNearby()) {
-            title = getResources().getString(R.string.nearby_text);
+        if (contact.isGroup()) {
+            if (contact.getGroupPresence().isTypeNearby()) {
+                title = getResources().getString(R.string.nearby_text);
+            } else {
+                title = contact.getName();
+            }
         } else {
-            title = contact.getName();
+            title = contact.getNickname();
         }
         mActionBar.setTitle(title);
     }
