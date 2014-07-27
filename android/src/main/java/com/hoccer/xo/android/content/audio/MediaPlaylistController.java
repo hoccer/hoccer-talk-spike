6e04cd6816a8497a7eae551ecdf2d62f7ca15def
Mirko Geissler <mirko.geissler@artcom.de>
2014-07-27 14:40:04 +0200

    Add listener support for certain events

--- android/src/main/java/com/hoccer/xo/android/content/audio/MediaPlaylistController.java
+++ android/src/main/java/com/hoccer/xo/android/content/audio/MediaPlaylistController.java
@@ -1,6 +1,7 @@
 package com.hoccer.xo.android.content.audio;
 
 import com.hoccer.talk.content.IContentObject;
+import com.hoccer.talk.util.WeakListenerArray;
 import com.hoccer.xo.android.content.EmptyPlaylist;
 import com.hoccer.xo.android.content.MediaPlaylist;
 import org.apache.log4j.Logger;
@@ -8,6 +9,13 @@ import org.apache.log4j.Logger;
 import java.util.*;
 
 public class MediaPlaylistController implements MediaPlaylist.Listener {
+
+    public interface Listener {
+        void onCurrentItemChanged(IContentObject newItem);
+        void onPlaylistChanged(MediaPlaylist newPlaylist);
+        void onRepeatModeChanged(RepeatMode newMode);
+        void onShuffleChanged(boolean isShuffled);
+    }
 
     public static enum RepeatMode {
         REPEAT_ITEM, REPEAT_ALL, NO_REPEAT;
@@ -26,6 +34,8 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
 
     private RepeatMode mRepeatMode = RepeatMode.NO_REPEAT;
 
+    WeakListenerArray<Listener> mListener = new WeakListenerArray<Listener>();
+
     public MediaPlaylistController() {
         reset();
     }
@@ -33,7 +43,7 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
     public void setCurrentIndex(int index) {
         if(isIndexInBounds(index)) {
             mCurrentIndex = mPlaylistOrder.indexOf(index);
-            mCurrentItem = mPlaylist.getItem(index);
+            setNewCurrentItem(mPlaylist.getItem(index));
             createPlaylistIndexes();
         } else {
             throw new IndexOutOfBoundsException("Index out of bounds");
@@ -102,14 +112,14 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
         switch (mRepeatMode) {
             case NO_REPEAT:
                 --mCurrentIndex;
-                mCurrentItem = mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex));
+                setNewCurrentItem(mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex)));
                 break;
             case REPEAT_ALL:
                 mCurrentIndex--;
                 if (mCurrentIndex < 0) {
                     mCurrentIndex = mPlaylist.size() - 1;
                 }
-                mCurrentItem = mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex));
+                setNewCurrentItem(mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex)));
                 break;
             case REPEAT_ITEM:
                 break; // do nothing
@@ -127,14 +137,14 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
         switch (mRepeatMode) {
             case NO_REPEAT:
                 ++mCurrentIndex;
-                mCurrentItem = mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex));
+                setNewCurrentItem(mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex)));
                 break;
             case REPEAT_ALL:
                 ++mCurrentIndex;
                 if (mCurrentIndex >= mPlaylist.size()) {
                     mCurrentIndex = 0;
                 }
-                mCurrentItem = mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex));
+                setNewCurrentItem(mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex)));
                 break;
             case REPEAT_ITEM:
                 break; // do nothing
@@ -161,19 +171,23 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
             } else {
                 mCurrentIndex = 0;
             }
-            mCurrentItem = mPlaylist.getItem(mCurrentIndex);
+            setNewCurrentItem(mPlaylist.getItem(mCurrentIndex));
         } else {
             mCurrentIndex = 0;
-            mCurrentItem = null;
+            setNewCurrentItem(null);
         }
 
         createPlaylistIndexes();
+
+        for(Listener listener : mListener) {
+            listener.onPlaylistChanged(mPlaylist);
+        }
     }
 
     public void reset() {
         setPlaylist(new EmptyPlaylist());
         mCurrentIndex = 0;
-        mCurrentItem = null;
+        setNewCurrentItem(null);
         createPlaylistIndexes();
     }
 
@@ -182,16 +196,32 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
     }
 
     public void setRepeatMode(RepeatMode repeatMode) {
+        if(mRepeatMode == repeatMode) {
+            return;
+        }
+
         this.mRepeatMode = repeatMode;
+
+        for(Listener listener : mListener) {
+            listener.onRepeatModeChanged(mRepeatMode);
+        }
     }
 
     public boolean getShuffleActive() {
         return mShuffleActive;
     }
 
-    public void setShuffleActive(boolean mShuffleActive) {
-        this.mShuffleActive = mShuffleActive;
+    public void setShuffleActive(boolean shuffleActive) {
+        if(mShuffleActive == shuffleActive) {
+            return;
+        }
+
+        mShuffleActive = shuffleActive;
         createPlaylistIndexes();
+
+        for(Listener listener : mListener) {
+            listener.onShuffleChanged(mShuffleActive);
+        }
     }
 
     private void createPlaylistIndexes() {
@@ -226,6 +256,14 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
         return (index >= 0) && (index < mPlaylist.size());
     }
 
+    public void registerListener(Listener listener) {
+        mListener.registerListener(listener);
+    }
+
+    public void unregisterListener(Listener listener) {
+        mListener.unregisterListener(listener);
+    }
+
     @Override
     public void onItemOrderChanged(MediaPlaylist playlist) {
         createPlaylistIndexes();
@@ -233,39 +271,49 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
 
     @Override
     public void onItemRemoved(MediaPlaylist playlist, IContentObject itemRemoved) {
-        if(mPlaylist.size() > 0 && mCurrentItem != null) {
-            // if the current title was removed
-            if(itemRemoved == mCurrentItem) {
-                switch (mRepeatMode) {
-                    case NO_REPEAT:
-                        if(mCurrentIndex < mPlaylist.size()) {
-                            mCurrentItem = mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex));
-                        } else {
+        if(mCurrentItem != null) {
+            if (mPlaylist.size() > 0) {
+                if (itemRemoved == mCurrentItem) {
+                    // the current title was removed move on to next based on repeat mode
+                    switch (mRepeatMode) {
+                        case NO_REPEAT:
+                            if (mCurrentIndex < mPlaylist.size()) {
+                                setNewCurrentItem(mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex)));
+                            } else {
+                                mCurrentIndex = -1;
+                                setNewCurrentItem(null);
+                            }
+                            break;
+                        case REPEAT_ALL:
+                            if (mCurrentIndex < mPlaylist.size()) {
+                                setNewCurrentItem(mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex)));
+                            } else {
+                                mCurrentIndex = 0;
+                                setNewCurrentItem(mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex)));
+                            }
+                            break;
+                        case REPEAT_ITEM:
                             mCurrentIndex = -1;
-                            mCurrentItem = null;
-                        }
-                        break;
-                    case REPEAT_ALL:
-                        if (mCurrentIndex < mPlaylist.size()) {
-                            mCurrentItem = mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex));
-                        } else {
-                            mCurrentIndex = 0;
-                            mCurrentItem = mPlaylist.getItem(mPlaylistOrder.get(mCurrentIndex));
-                        }
-                        break;
-                    case REPEAT_ITEM:
-                        mCurrentIndex = -1;
-                        mCurrentItem = null;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Invalid repeat mode encountered");
+                            setNewCurrentItem(null);
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Invalid repeat mode encountered");
+                    }
                 }
+            } else {
+                mCurrentIndex = -1;
+                setNewCurrentItem(null);
             }
-        } else {
-            mCurrentIndex = -1;
-            mCurrentItem = null;
+            createPlaylistIndexes();
         }
-        createPlaylistIndexes();
+    }
+
+    private void setNewCurrentItem(IContentObject newItem) {
+        mCurrentItem = newItem;
+
+        for(Listener listener : mListener) {
+            listener.onCurrentItemChanged(mCurrentItem);
+        }
     }
 
     @Override
@@ -276,7 +324,7 @@ public class MediaPlaylistController implements MediaPlaylist.Listener {
     @Override
     public void onPlaylistCleared(MediaPlaylist playlist) {
         mCurrentIndex = -1;
-        mCurrentItem = null;
+        setNewCurrentItem(null);
         createPlaylistIndexes();
     }
 }
