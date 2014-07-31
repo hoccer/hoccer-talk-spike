58ac0011d69efe2dec07e46c076b7647c560c28b
Nico Nußbaum <nico@artcom.de>
2014-07-31 10:57:21 +0200

    Handle removal and renaming of collections through dialogs

--- android/src/main/java/com/hoccer/xo/android/fragment/MediaCollectionListFragment.java
+++ android/src/main/java/com/hoccer/xo/android/fragment/MediaCollectionListFragment.java
@@ -1,16 +1,21 @@
 package com.hoccer.xo.android.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.*;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import com.hoccer.talk.client.model.TalkClientMediaCollection;
+import com.hoccer.xo.android.XoApplication;
+import com.hoccer.xo.android.XoDialogs;
 import com.hoccer.xo.android.util.AttachmentOperationHelper;
 import com.hoccer.xo.release.R;
-import com.mobeta.android.dslv.DragSortListView;
 import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
 
 public class MediaCollectionListFragment extends BaseMediaCollectionListFragment {
 
@@ -22,13 +27,12 @@ public class MediaCollectionListFragment extends BaseMediaCollectionListFragment
         View view = inflater.inflate(R.layout.fragment_media_collection_list, container, false);
 
         return view;
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         ListInteractionHandler handler = new ListInteractionHandler();
         getListView().setMultiChoiceModeListener(handler);
         getListView().setOnItemClickListener(handler);
@@ -75,22 +79,78 @@ public class MediaCollectionListFragment extends BaseMediaCollectionListFragment
 
         @Override
         public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            boolean eventHandled = false;
+            TalkClientMediaCollection selectedCollection = (TalkClientMediaCollection) getListView().getSelectedItem();
             switch (menuItem.getItemId()) {
                 case R.id.menu_delete_collection:
-
+                    DeleteCollectionCallbackHandler deleteHandler = new DeleteCollectionCallbackHandler(selectedCollection);
+                    XoDialogs.showYesNoDialog("delete_collection", R.string.dialog_delete_collection_title,
+                            R.string.dialog_delete_collection_message, getActivity(), deleteHandler, new CancelDialogCallbackHandler());
+                    eventHandled = true;
                     break;
                 case R.id.menu_rename_collection:
-
+                    RenameCollectionCallbackHandler renameHandler = new RenameCollectionCallbackHandler(selectedCollection);
+                    XoDialogs.showInputTextDialog("rename_collection", R.string.rename_collection, 0, getActivity(),
+                            renameHandler, new CancelDialogCallbackHandler());
+                    eventHandled = true;
                     break;
                 default:
-
             }
-            return false;
+
+            return eventHandled;
         }
 
         @Override
         public void onDestroyActionMode(ActionMode actionMode) {
 
+        }
+
+        private class DeleteCollectionCallbackHandler implements DialogInterface.OnClickListener {
+
+            private TalkClientMediaCollection mCollection;
+
+            DeleteCollectionCallbackHandler(TalkClientMediaCollection collection) {
+                mCollection = collection;
+            }
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                try {
+                    mCollection.clear();
+                    XoApplication.getXoClient().getDatabase().deleteMediaCollection(mCollection);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage());
+                }
+            }
+        }
+
+        private class RenameCollectionCallbackHandler implements XoDialogs.OnTextClickListener {
+
+            private TalkClientMediaCollection mCollection;
+
+            RenameCollectionCallbackHandler(TalkClientMediaCollection collection) {
+                mCollection = collection;
+            }
+
+            @Override
+            public void onClick(DialogInterface dialog, int id, String password) {
+                if (password != null && !password.isEmpty()) {
+                    mCollection.setName(password);
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((BaseAdapter) getListAdapter()).notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }
+
+        private class CancelDialogCallbackHandler implements DialogInterface.OnClickListener {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
         }
     }
 }
