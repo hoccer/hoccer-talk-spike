cf8958b6f98b3603ca1b5da64d5916fdf3577d7d
Jens Eraßmy <jens.erassmy@artcom.de>
2014-08-21 17:57:43 +0200

    Use picasso for loading images in chat view instead of using Thumbnail Manager

--- android/src/main/java/com/hoccer/xo/android/view/chat/attachments/ChatImageItem.java
+++ android/src/main/java/com/hoccer/xo/android/view/chat/attachments/ChatImageItem.java
@@ -1,36 +1,24 @@
 package com.hoccer.xo.android.view.chat.attachments;
 
-import android.content.ContentUris;
-import android.database.Cursor;
 import android.graphics.*;
-import android.media.ThumbnailUtils;
-import android.provider.MediaStore;
-import android.view.WindowManager;
-import com.hoccer.talk.client.model.TalkClientDownload;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
+import android.view.*;
 import com.hoccer.talk.client.model.TalkClientMessage;
 import com.hoccer.talk.content.IContentObject;
 import com.hoccer.xo.android.XoApplication;
 import com.hoccer.xo.android.base.XoActivity;
-import com.hoccer.xo.android.util.ThumbnailManager;
 import com.hoccer.xo.android.view.chat.ChatMessageItem;
 import com.hoccer.xo.release.R;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.sql.SQLException;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 
 public class ChatImageItem extends ChatMessageItem {
@@ -83,8 +71,78 @@ public class ChatImageItem extends ChatMessageItem {
         }
         if (contentObject.getContentDataUrl() != null) {
             mAttachmentView.setBackgroundDrawable(null);
-            ThumbnailManager.getInstance(mContext).displayThumbnailForImage(contentObject.getContentDataUrl(), imageView, mask, messageTag);
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(contentObject.getContentDataUrl(), options);
+
+            double aspectRatio = contentObject.getContentAspectRatio();
+
+            WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            int width = (int) (size.x * 0.8);
+            int height = (int) (width / aspectRatio);
+
+//            Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), mask);
+//            byte[] chunk = bitmap.getNinePatchChunk();
+//            NinePatchDrawable drawable = new NinePatchDrawable(mContext.getResources(), bitmap, chunk, new Rect(), null);
+//            drawable.setBounds(0, 0, width, height);
+
+            Picasso.with(mContext).setIndicatorsEnabled(XoApplication.getConfiguration().isDevelopmentModeEnabled());
+            Picasso.with(mContext)
+                    .load(contentObject.getContentDataUrl())
+//                    .placeholder(drawable)
+                    .resize(width, height)
+                    .transform(new BubbleTransformation(mask))
+                    .into(imageView);
+
+//            ThumbnailManager.getInstance(mContext).displayThumbnailForImage(contentObject.getContentDataUrl(), imageView, mask, messageTag);
         }
+    }
+
+    private class BubbleTransformation implements Transformation {
+
+        private int mask;
+
+        public BubbleTransformation(int mask) {
+            this.mask = mask;
+        }
+
+        @Override
+        public Bitmap transform(Bitmap source) {
+            Bitmap mask = getNinePatchMask(this.mask, source.getWidth(), source.getHeight(), mContext);
+            Bitmap result = Bitmap.createBitmap(source.getWidth(), source.getHeight(), Bitmap.Config.ARGB_8888);
+
+            Canvas c = new Canvas(result);
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+            c.drawBitmap(source, 0, 0, null);
+            c.drawBitmap(mask, 0, 0, paint);
+
+            paint.setXfermode(null);
+            if (result != source) {
+                source.recycle();
+            }
+            return result;
+        }
+
+        @Override
+        public String key() {
+            return "square()";
+        }
+    }
+
+    private Bitmap getNinePatchMask(int id, int x, int y, Context context) {
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), id);
+        byte[] chunk = bitmap.getNinePatchChunk();
+        NinePatchDrawable drawable = new NinePatchDrawable(context.getResources(), bitmap, chunk, new Rect(), null);
+        drawable.setBounds(0, 0, x, y);
+        Bitmap result = Bitmap.createBitmap(x, y, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(result);
+        drawable.draw(canvas);
+        return result;
     }
 
     private void displayImage(IContentObject contentObject) {
