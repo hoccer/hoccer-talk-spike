fb8ac52b41811d2ac6fda52db30ab053b1fba58d
Julian Krumow <julian.krumow@artcom.de>
2014-08-26 16:12:09 +0200

    Revert "Moves Upload? and Download? text into ChatMessageItem."
    This reverts commit a4152be434552ad02c18055af8b3c91c4dcdf775.

--- android/src/main/java/com/hoccer/xo/android/view/chat/attachments/AttachmentTransferHandler.java
+++ android/src/main/java/com/hoccer/xo/android/view/chat/attachments/AttachmentTransferHandler.java
@@ -176,7 +176,9 @@ public class AttachmentTransferHandler implements View.OnClickListener, IXoTrans
                         progress = mContent.getTransferProgress();
                         mTransferControl.setMax(length);
                         mTransferControl.setProgressImmediately(progress);
-                        mTransferControl.setText("");
+                        TalkClientDownload download = (TalkClientDownload)mContent;
+                        fileSize = Formatter.formatShortFileSize(mTransferControl.getContext(), download.getTransmittedContentLength());
+                        mTransferControl.setText(res.getString(R.string.attachment_on_hold_download_question, fileSize));
                         mTransferControl.prepareToDownload();
                         mTransferControl.pause();
                         break;
@@ -235,7 +237,8 @@ public class AttachmentTransferHandler implements View.OnClickListener, IXoTrans
                         progress = mContent.getTransferProgress();
                         mTransferControl.setMax(length);
                         mTransferControl.setProgressImmediately(progress);
-                        mTransferControl.setText("");
+                        fileSize = Formatter.formatShortFileSize(mTransferControl.getContext(), mContent.getContentLength());
+                        mTransferControl.setText(res.getString(R.string.attachment_on_hold_upload_question, fileSize));
                         mTransferControl.pause();
                         break;
 
