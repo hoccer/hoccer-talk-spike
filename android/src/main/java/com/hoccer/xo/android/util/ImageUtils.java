6048d600bd4c7368901982f204e00279aad73f99
Nico Nußbaum <nico@artcom.de>
2014-09-04 18:51:29 +0200

    Start changing encodeBitmap() to handle a list of images

--- android/src/main/java/com/hoccer/xo/android/util/ImageUtils.java
+++ android/src/main/java/com/hoccer/xo/android/util/ImageUtils.java
@@ -3,16 +3,25 @@ package com.hoccer.xo.android.util;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
+import com.hoccer.xo.android.XoApplication;
 import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ImageUtils {
+
+    public static final String MIME_TYPE_IMAGE_PREFIX = "image/";
 
     private static Logger LOG = Logger.getLogger(ImageUtils.class);
 
@@ -91,5 +100,119 @@ public class ImageUtils {
             result.y = maxHeight;
         }
         return result;
+    }
+
+
+    public static void encodeBitmap(final ArrayList<File> input, final File out, final int maxPixelCount, final int imageQuality,
+                                    final Bitmap.CompressFormat format, final EncodingCompleteCallback callback) {
+
+
+        AsyncTask<Void, Void, Boolean> encodingTask = new AsyncTask<Void, Void, Boolean>() {
+
+            @Override
+            protected Boolean doInBackground(Void[] params) {
+                boolean encodingSuccessful = false;
+
+                for (File in : input) {
+                    FileOutputStream outStream = null;
+
+                    try {
+                        BitmapFactory.Options encodingOptions = new BitmapFactory.Options();
+                        encodingOptions.inJustDecodeBounds = true;
+                        BitmapFactory.decodeFile(in.getAbsolutePath(), encodingOptions);
+
+                        long originalPixelCount = encodingOptions.outWidth * encodingOptions.outHeight;
+                        if (maxPixelCount < originalPixelCount) {
+                            double resizeRatio = Math.sqrt(originalPixelCount / maxPixelCount);
+                            encodingOptions.inSampleSize = (int) resizeRatio + 1;
+                            encodingOptions.outWidth = (int) (encodingOptions.outWidth / resizeRatio);
+                            encodingOptions.outHeight = (int) (encodingOptions.outHeight / resizeRatio);
+                        }
+                        encodingOptions.inJustDecodeBounds = false;
+                        Bitmap encodedImage = BitmapFactory.decodeFile(in.getAbsolutePath(), encodingOptions);
+                        outStream = new FileOutputStream(out);
+
+                        Bitmap.CompressFormat compressFormat = getFormatByMimeType(encodingOptions.outMimeType);
+
+                        boolean shallCopyExif = false;
+                        if (compressFormat == null) {
+                            compressFormat = format;
+                        } else {
+                            if (compressFormat == Bitmap.CompressFormat.JPEG) {
+                                shallCopyExif = true;
+                            }
+                        }
+
+                        encodingSuccessful = encodedImage.compress(compressFormat, imageQuality, outStream);
+
+                        if (shallCopyExif) {
+                            copyExifData(in.getAbsolutePath(), out.getAbsolutePath());
+                        }
+
+                    } catch (FileNotFoundException e) {
+                        LOG.error("Fatal error in creating temporary file " + out.getPath(), e);
+                    } finally {
+                        try {
+                            if (outStream != null) {
+                                outStream.close();
+                            }
+                        } catch (IOException e) {
+                            LOG.error("Fatal error while closing output stream for " + out.getPath(), e);
+                        }
+                    }
+                }
+                return encodingSuccessful;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                super.onPostExecute(result);
+
+                if (result) {
+                    callback.onSuccessfulEncoding(null);
+                } else {
+                    callback.onError();
+                }
+            }
+        };
+
+        encodingTask.execute();
+    }
+
+    public static Bitmap.CompressFormat getFormatByMimeType(String mimeType) {
+        Bitmap.CompressFormat format = null;
+        if (mimeType != null || !mimeType.isEmpty()) {
+            if (mimeType.equalsIgnoreCase(MIME_TYPE_IMAGE_PREFIX + Bitmap.CompressFormat.JPEG.name())) {
+                format = Bitmap.CompressFormat.JPEG;
+            } else if (mimeType.equalsIgnoreCase(MIME_TYPE_IMAGE_PREFIX + Bitmap.CompressFormat.PNG.name())) {
+                format = Bitmap.CompressFormat.PNG;
+            } else if (mimeType.equalsIgnoreCase(MIME_TYPE_IMAGE_PREFIX + Bitmap.CompressFormat.WEBP.name())) {
+                format = Bitmap.CompressFormat.WEBP;
+            }
+        }
+
+        return format;
+    }
+
+    public static boolean copyExifData(String inPath, String outPath) {
+        boolean success = false;
+        try {
+            ExifInterface exifIn = new ExifInterface(inPath);
+            ExifInterface exifOut = new ExifInterface(outPath);
+            if (exifIn != null && exifOut != null) {
+                exifOut.setAttribute(ExifInterface.TAG_ORIENTATION, exifIn.getAttribute(ExifInterface.TAG_ORIENTATION));
+                exifOut.saveAttributes();
+            }
+        } catch (IOException e) {
+            LOG.error("Error loading Exif data", e);
+        }
+
+        return success;
+    }
+
+    public interface EncodingCompleteCallback {
+        public void onSuccessfulEncoding(ArrayList<File> files);
+
+        public void onError();
     }
 }
