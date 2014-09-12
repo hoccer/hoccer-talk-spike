bce5ee130218a56ea843cbb90946fbb3f8f42431
Jens Eraßmy <jens.erassmy@artcom.de>
2014-09-12 12:02:07 +0200

    Initialize android test module to support UI testing

---
+++ android-test/src/test/java/com/hoccer/xo/android/activity/SingleProfileActivityTest.java
@@ -0,0 +1,17 @@
+package com.hoccer.xo.android.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import junit.framework.Assert;
+
+public class SingleProfileActivityTest extends ActivityInstrumentationTestCase2<SingleProfileActivity> {
+
+    public SingleProfileActivityTest(Class<SingleProfileActivity> activityClass) {
+        super(activityClass);
+    }
+
+    public void testSomething() throws Exception {
+        SingleProfileActivity activity = getActivity();
+        String title = (String) activity.getTitle();
+        Assert.assertEquals("My profile", title);
+    }
+}
