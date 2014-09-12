663d93a5560eacd725a1228f8b65a76b4627fc46
Jens Eraßmy <jens.erassmy@artcom.de>
2014-09-12 17:20:17 +0200

    Fix compilation

--- android-test/src/main/java/com/hoccer/xo/android/activity/SingleProfileActivityTest.java
+++ android-test/src/main/java/com/hoccer/xo/android/activity/SingleProfileActivityTest.java
@@ -5,8 +5,16 @@ import junit.framework.Assert;
 
 public class SingleProfileActivityTest extends ActivityInstrumentationTestCase2<SingleProfileActivity> {
 
-    public SingleProfileActivityTest(Class<SingleProfileActivity> activityClass) {
-        super(activityClass);
+//    public SingleProfileActivityTest(Class<SingleProfileActivity> activityClass) {
+//        super(activityClass);
+//    }
+
+//    public SingleProfileActivityTest(String pkg, Class<SingleProfileActivity> activityClass) {
+//        super(pkg, activityClass);
+//    }
+
+    public SingleProfileActivityTest() {
+        super(SingleProfileActivity.class);
     }
 
     public void testSomething() throws Exception {
