ec187a272795c791252a98fe0a8380086f27184f
Jens Eraßmy <jens.erassmy@artcom.de>
2014-09-17 11:56:46 +0200

    Use standard android instrumentation tests due to espresso dependency problems

--- android-test/src/main/java/com/hoccer/xo/android/activity/SingleProfileActivityTest.java
+++ android-test/src/main/java/com/hoccer/xo/android/activity/SingleProfileActivityTest.java
@@ -1,15 +1,27 @@
 package com.hoccer.xo.android.activity;
 
 import android.test.ActivityInstrumentationTestCase2;
+//import com.google.android.apps.common.testing.ui.espresso.Espresso;
+//import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
+//import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 import junit.framework.Assert;
+//import org.hamcrest.Matchers;
+
 
 public class SingleProfileActivityTest extends ActivityInstrumentationTestCase2<SingleProfileActivity> {
 
-    public SingleProfileActivityTest(Class<SingleProfileActivity> activityClass) {
-        super(activityClass);
+    public SingleProfileActivityTest() {
+        super(SingleProfileActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
     }
 
     public void testSomething() throws Exception {
+
+//        Espresso.onView(ViewMatchers.withId(com.hoccer.xo.release.R.id.tv_profile_name_text)).check(ViewAssertions.matches(ViewMatchers.withText(Matchers.containsString("Name"))));
 
         SingleProfileActivity activity = getActivity();
         String title = (String) activity.getTitle();
