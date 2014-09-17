f4cf82cc466fa76329eeaabf48a82f9b47cd970c
Guido Lorenz <guido.lorenz@artcom.de>
2014-09-17 17:03:16 +0200

    Create fragment programmatically
    This will allow us to replace the fragment later.

--- android/src/main/java/com/hoccer/xo/android/activity/QrCodeActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/QrCodeActivity.java
@@ -1,13 +1,16 @@
 package com.hoccer.xo.android.activity;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import com.hoccer.xo.android.base.XoActionbarActivity;
+import com.hoccer.xo.android.fragment.QrCodeScannerFragment;
 import com.hoccer.xo.release.R;
 
 public class QrCodeActivity extends XoActionbarActivity {
     @Override
     protected int getLayoutResource() {
-        return R.layout.activity_qr_code;
+        return R.layout.default_framelayout;
     }
 
     @Override
@@ -19,5 +22,9 @@ public class QrCodeActivity extends XoActionbarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         enableUpNavigation();
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fl_fragment_container, new QrCodeScannerFragment());
+        transaction.commit();
     }
 }
