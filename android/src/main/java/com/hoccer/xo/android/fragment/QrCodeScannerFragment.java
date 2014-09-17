c01c9c144c7ee29a1b7b750b07cdc0851e633815
Guido Lorenz <guido.lorenz@artcom.de>
2014-09-17 11:05:29 +0200

    Add new QR code scanner fragment
    Eventually it will replace QrScannerActivity.

---
+++ android/src/main/java/com/hoccer/xo/android/fragment/QrCodeScannerFragment.java
@@ -0,0 +1,17 @@
+package com.hoccer.xo.android.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.hoccer.xo.release.R;
+
+public class QrCodeScannerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_qr_code_scanner, null);
+        return view;
+    }
+}
