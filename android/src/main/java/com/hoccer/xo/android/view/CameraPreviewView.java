cea00ca91a2c39d9339dd702e476407df91db2e1
Guido Lorenz <guido.lorenz@artcom.de>
2014-09-17 15:52:20 +0200

    Add logging to camera preview

--- android/src/main/java/com/hoccer/xo/android/view/CameraPreviewView.java
+++ android/src/main/java/com/hoccer/xo/android/view/CameraPreviewView.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import org.apache.log4j.Logger;
 
 public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final Logger LOG = Logger.getLogger(CameraPreviewView.class);
+
     private final Camera mCamera;
     private final Camera.PreviewCallback mPreviewCallback;
     private final boolean mAutoFocus;
@@ -21,16 +25,18 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        // do nothing
+        LOG.debug("surfaceCreated()");
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // do nothing
+        LOG.debug("surfaceDestroyed()");
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        LOG.debug("surfaceChanged()");
+
         if (holder.getSurface() == null) {
             return;
         }
@@ -43,7 +49,7 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
             mCamera.setPreviewCallback(null);
             mCamera.stopPreview();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Error stopping camera preview", e);
         }
 
         try {
@@ -56,7 +62,7 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
                 mCamera.autoFocus(null);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Error starting camera preview", e);
         }
     }
 }
