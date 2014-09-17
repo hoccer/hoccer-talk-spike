d82927ebc1b6d8ea09e3d444a5293f6d77bc7f89
Guido Lorenz <guido.lorenz@artcom.de>
2014-09-17 12:33:25 +0200

    Reimplement QR code scanning and pairing

--- android/src/main/java/com/hoccer/xo/android/fragment/QrCodeScannerFragment.java
+++ android/src/main/java/com/hoccer/xo/android/fragment/QrCodeScannerFragment.java
@@ -5,9 +5,14 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+import com.hoccer.xo.android.XoApplication;
 import com.hoccer.xo.android.view.CameraPreviewView;
 import com.hoccer.xo.release.R;
+import net.sourceforge.zbar.*;
 import org.apache.log4j.Logger;
+
+import java.util.HashSet;
 
 public class QrCodeScannerFragment extends Fragment {
 
@@ -16,12 +21,60 @@ public class QrCodeScannerFragment extends Fragment {
     private Camera mCamera;
     private FrameLayout mCameraPreviewLayout;
 
+    private final ImageScanner mQrCodeScanner = new ImageScanner();
+    private final HashSet<String> mScannedCodes = new HashSet<String>();
+
+    private Camera.PreviewCallback mPreviewCallback = new Camera.PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            Camera.Parameters parameters = camera.getParameters();
+            Camera.Size size = parameters.getPreviewSize();
+
+            Image image = new Image(size.width, size.height, "Y800");
+            image.setData(data);
+
+            int result = mQrCodeScanner.scanImage(image);
+            if (result != 0) {
+                SymbolSet symbols = mQrCodeScanner.getResults();
+
+                for (Symbol symbol : symbols) {
+                    String code = symbol.getData();
+
+                    if (!mScannedCodes.contains(code)) {
+                        if (code.startsWith(XoApplication.getXoClient().getConfiguration().getUrlScheme())) {
+                            String pairingToken = code.replace(XoApplication.getXoClient().getConfiguration().getUrlScheme(), "");
+                            XoApplication.getXoClient().performTokenPairing(pairingToken);
+                        } else {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getActivity(), getResources().getString(R.string.toast_pairing_failed), Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+
+                        mScannedCodes.add(code);
+                    }
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mQrCodeScanner.setConfig(0, Config.X_DENSITY, 3);
+        mQrCodeScanner.setConfig(0, Config.Y_DENSITY, 3);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_qr_code_scanner, null);
-        mCameraPreviewLayout = (FrameLayout)view.findViewById(R.id.fl_camera_preview);
+        return inflater.inflate(R.layout.fragment_qr_code_scanner, null);
+    }
 
-        return view;
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mCameraPreviewLayout = (FrameLayout)view.findViewById(R.id.fl_camera_preview);
     }
 
     @Override
@@ -29,7 +82,7 @@ public class QrCodeScannerFragment extends Fragment {
         super.onResume();
         mCamera = openCamera();
 
-        CameraPreviewView cameraPreview = new CameraPreviewView(getActivity(), mCamera, null, null);
+        CameraPreviewView cameraPreview = new CameraPreviewView(getActivity(), mCamera, mPreviewCallback, null);
         mCameraPreviewLayout.addView(cameraPreview);
     }
 
