7cdbe78524d2b1a3d6c4305ec1787abfc211349c
Guido Lorenz <guido.lorenz@artcom.de>
2014-09-17 12:08:11 +0200

    Add QR code camera preview

--- android/src/main/java/com/hoccer/xo/android/fragment/QrCodeScannerFragment.java
+++ android/src/main/java/com/hoccer/xo/android/fragment/QrCodeScannerFragment.java
@@ -1,17 +1,59 @@
 package com.hoccer.xo.android.fragment;
 
 import android.app.Fragment;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
+import android.widget.FrameLayout;
+import com.hoccer.xo.android.view.CameraPreviewView;
 import com.hoccer.xo.release.R;
+import org.apache.log4j.Logger;
 
 public class QrCodeScannerFragment extends Fragment {
+
+    private static final Logger LOG = Logger.getLogger(QrCodeScannerFragment.class);
+
+    private Camera mCamera;
+    private FrameLayout mCameraPreviewLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_qr_code_scanner, null);
+        mCameraPreviewLayout = (FrameLayout)view.findViewById(R.id.fl_camera_preview);
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCamera = openCamera();
+
+        CameraPreviewView cameraPreview = new CameraPreviewView(getActivity(), mCamera, null, null);
+        mCameraPreviewLayout.addView(cameraPreview);
+    }
+
+    private Camera openCamera() {
+        try {
+            return Camera.open();
+        } catch (Exception e) {
+            LOG.error("Error opening camera", e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mCameraPreviewLayout.removeAllViews();
+
+        closeCamera(mCamera);
+        mCamera = null;
+    }
+
+    private void closeCamera(Camera camera) {
+        camera.setPreviewCallback(null);
+        camera.release();
     }
 }
---
+++ android/src/main/java/com/hoccer/xo/android/view/CameraPreviewView.java
@@ -0,0 +1,64 @@
+package com.hoccer.xo.android.view;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Callback {
+    private final Camera mCamera;
+    private final Camera.PreviewCallback mPreviewCallback;
+    private final Camera.AutoFocusCallback mAutoFocusCallback;
+
+    public CameraPreviewView(Context context, Camera camera, Camera.PreviewCallback previewCallback, Camera.AutoFocusCallback autoFocusCallback) {
+        super(context);
+        mCamera = camera;
+        mPreviewCallback = previewCallback;
+        mAutoFocusCallback = autoFocusCallback;
+
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // do nothing
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // do nothing
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (holder.getSurface() == null) {
+            return;
+        }
+
+        try {
+            if (mAutoFocusCallback != null) {
+                mCamera.cancelAutoFocus();
+            }
+
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mCamera.setDisplayOrientation(90);
+            mCamera.setPreviewDisplay(holder);
+            mCamera.setPreviewCallback(mPreviewCallback);
+            mCamera.startPreview();
+
+            if (mAutoFocusCallback != null) {
+                mCamera.autoFocus(mAutoFocusCallback);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
