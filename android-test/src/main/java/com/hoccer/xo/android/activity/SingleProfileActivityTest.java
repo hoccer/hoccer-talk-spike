35766281b49778b4e47df5a044719990d6fb7af4
Jens Eraßmy <jens.erassmy@artcom.de>
2014-09-18 13:10:31 +0200

    Add test for profile creation

--- android-test/src/main/java/com/hoccer/xo/android/activity/SingleProfileActivityTest.java
+++ android-test/src/main/java/com/hoccer/xo/android/activity/SingleProfileActivityTest.java
@@ -1,9 +1,23 @@
 package com.hoccer.xo.android.activity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.test.ActivityInstrumentationTestCase2;
-import junit.framework.Assert;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.hoccer.xo.android.XoApplication;
+import com.hoccer.xo.android.fragment.SingleProfileFragment;
+import org.apache.log4j.Logger;
+
 
 public class SingleProfileActivityTest extends ActivityInstrumentationTestCase2<SingleProfileActivity> {
+
+    private static final Logger LOG = Logger.getLogger(SingleProfileActivity.class);
+
+    private SingleProfileActivity activity;
+    private SingleProfileFragment singleProfileFragment;
 
     public SingleProfileActivityTest() {
         super(SingleProfileActivity.class);
@@ -11,12 +25,59 @@ public class SingleProfileActivityTest extends ActivityInstrumentationTestCase2<
 
     public void setUp() throws Exception {
         super.setUp();
-        getActivity();
+
+        Intent intent = new Intent();
+        intent.putExtra(SingleProfileActivity.EXTRA_CLIENT_CREATE_SELF, true);
+        setActivityIntent(intent);
+
+        activity = getActivity();
+        singleProfileFragment = (SingleProfileFragment) getActivity().getSupportFragmentManager().
+                findFragmentByTag(SingleProfileActivity.SINGLE_PROFILE_FRAGMENT);
     }
 
-    public void testSomething() throws Exception {
-        SingleProfileActivity activity = getActivity();
-        String title = (String) activity.getTitle();
-        Assert.assertEquals("Hoccer", title);
+    public void testPreConditions() {
+        assertTrue(activity.getIntent().hasExtra(SingleProfileActivity.EXTRA_CLIENT_CREATE_SELF));
+        assertNotNull(singleProfileFragment);
+        assertTrue(singleProfileFragment.isAdded());
+        assertTrue(singleProfileFragment.isVisible());
+        assertTrue(singleProfileFragment.getArguments().getBoolean(SingleProfileFragment.ARG_CREATE_SELF));
+    }
+
+    public void testActionModeUI() {
+        TextView nameText = (TextView) singleProfileFragment.getView().findViewById(com.hoccer.xo.release.R.id.tv_profile_name);
+        EditText editName = (EditText) singleProfileFragment.getView().findViewById(com.hoccer.xo.release.R.id.et_profile_name);
+        RelativeLayout keyContainer = (RelativeLayout) singleProfileFragment.getView().findViewById(com.hoccer.xo.release.R.id.inc_profile_key);
+
+        assertEquals(keyContainer.getVisibility(), View.GONE);
+        assertEquals(nameText.getVisibility(), View.INVISIBLE);
+        assertEquals(editName.getVisibility(), View.VISIBLE);
+        assertEquals(editName.getHint(), activity.getString(com.hoccer.xo.release.R.string.profile_name_hint));
+        assertEquals(editName.getText().toString(), "");
+    }
+
+    public void testOnDestroyActionMode() {
+        final EditText editName = (EditText) singleProfileFragment.getView().findViewById(com.hoccer.xo.release.R.id.et_profile_name);
+        final String expectedName = "myName";
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                editName.setText(expectedName);
+
+            }
+        });
+
+        int closeButtonId = Resources.getSystem().getIdentifier("action_mode_close_button", "id", "android");
+        final View closeButton = getActivity().findViewById(closeButtonId);
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                closeButton.performClick();
+
+            }
+        });
+
+        assertEquals(expectedName, editName.getText().toString());
+        assertEquals(expectedName, XoApplication.getXoClient().getSelfContact().getSelf().getRegistrationName());
+        assertTrue(activity.isFinishing());
     }
 }
