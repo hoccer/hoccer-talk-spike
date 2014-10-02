64f767fab0cab89dccaefc874bcb46047384801f
Jens Eraßmy <jens.erassmy@artcom.de>
2014-10-02 14:38:18 +0200

    Set placeholder image for empty list

--- android/src/main/java/com/hoccer/xo/android/fragment/ClientListFragment.java
+++ android/src/main/java/com/hoccer/xo/android/fragment/ClientListFragment.java
@@ -3,18 +3,21 @@ package com.hoccer.xo.android.fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.hoccer.talk.client.IXoContactListener;
 import com.hoccer.talk.client.model.TalkClientContact;
 import com.hoccer.xo.android.XoApplication;
 import com.hoccer.xo.android.activity.SingleProfileActivity;
-import com.hoccer.xo.android.adapter.ClientContactsAdapter;
+import com.hoccer.xo.android.adapter.ClientsAdapter;
+import com.hoccer.xo.android.util.ColorSchemeManager;
 import com.hoccer.xo.release.R;
 
 import java.sql.SQLException;
@@ -23,11 +26,34 @@ public class ClientListFragment extends ListFragment implements IPagerFragment, 
 
 
     ClientsAdapter mClientsAdapter;
+    private ImageView mPlaceholderImageFrame;
+    private ImageView mPlaceholderImage;
 
     private View tabView;
     private TextView notificationBadgeTextView;
 
     private int mInvitedMeCount = 0;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mPlaceholderImageFrame = (ImageView) view.findViewById(R.id.iv_contacts_placeholder_frame);
+        mPlaceholderImage = (ImageView) view.findViewById(R.id.iv_contacts_placeholder);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mPlaceholderImageFrame.setBackground(getResources().getDrawable(R.drawable.placeholder_chats));
+            mPlaceholderImage.setBackground(ColorSchemeManager.getRepaintedDrawable(getActivity(), R.drawable.placeholder_chats_head, true));
+        } else {
+            mPlaceholderImageFrame.setBackgroundDrawable(getResources().getDrawable(R.drawable.placeholder_chats));
+            mPlaceholderImage.setBackgroundDrawable(ColorSchemeManager.getRepaintedDrawable(getActivity(), R.drawable.placeholder_chats_head, true));
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,6 +87,15 @@ public class ClientListFragment extends ListFragment implements IPagerFragment, 
     }
 
     @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        TalkClientContact contact = (TalkClientContact) getListAdapter().getItem(position);
+        Intent intent = new Intent(getActivity(), SingleProfileActivity.class);
+        intent.putExtra(SingleProfileActivity.EXTRA_CLIENT_CONTACT_ID, contact.getClientContactId());
+        startActivity(intent);
+    }
+
+    @Override
     public void onPageSelected() {
 
     }
@@ -73,15 +108,6 @@ public class ClientListFragment extends ListFragment implements IPagerFragment, 
     @Override
     public void onPageScrollStateChanged(int state) {
 
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        TalkClientContact contact = (TalkClientContact) getListAdapter().getItem(position);
-        Intent intent = new Intent(getActivity(), SingleProfileActivity.class);
-        intent.putExtra(SingleProfileActivity.EXTRA_CLIENT_CONTACT_ID, contact.getClientContactId());
-        startActivity(intent);
     }
 
     @Override
