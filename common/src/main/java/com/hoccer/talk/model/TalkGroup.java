9dc69266905e531ae97fec27b7fb669afe383f5c
Mirko Geissler <mirko.geissler@artcom.de>
2014-10-02 15:46:47 +0200

    Add missing groupTag setting in TalkGroup

--- common/src/main/java/com/hoccer/talk/model/TalkGroup.java
+++ common/src/main/java/com/hoccer/talk/model/TalkGroup.java
@@ -203,6 +203,11 @@ public class TalkGroup {
             updated = true;
         }
 
+        if(!groupTag.equals(g.groupTag)) {
+            groupTag = g.groupTag;
+            updated = true;
+        }
+
         if(!groupAvatarUrl.equals(g.groupAvatarUrl)) {
             groupAvatarUrl = g.groupAvatarUrl;
             updated = true;
