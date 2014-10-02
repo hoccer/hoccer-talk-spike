a02c1e34f09ad1f2060f67d7a0ec0c12a5211feb
Jens Eraßmy <jens.erassmy@artcom.de>
2014-10-02 14:21:34 +0200

    Integrate custom view pager tab for pending invitations notification

--- android/src/main/java/com/hoccer/xo/android/fragment/ClientListFragment.java
+++ android/src/main/java/com/hoccer/xo/android/fragment/ClientListFragment.java
@@ -5,35 +5,58 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
+import com.hoccer.talk.client.IXoContactListener;
 import com.hoccer.talk.client.model.TalkClientContact;
 import com.hoccer.xo.android.XoApplication;
 import com.hoccer.xo.android.activity.SingleProfileActivity;
 import com.hoccer.xo.android.adapter.ClientContactsAdapter;
 import com.hoccer.xo.release.R;
 
-public class ClientListFragment extends ListFragment implements IPagerFragment {
+import java.sql.SQLException;
+
+public class ClientListFragment extends ListFragment implements IPagerFragment, IXoContactListener {
 
     ClientContactsAdapter mClientContactsAdapter;
+
+    private View tabView;
+    private TextView notificationBadgeTextView;
+
+    private int mInvitedMeCount = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mClientContactsAdapter = new ClientContactsAdapter(getActivity());
+        notificationBadgeTextView = (TextView) tabView.findViewById(R.id.tv_contact_invite_notification_badge);
         setListAdapter(mClientContactsAdapter);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        try {
+            mInvitedMeCount = (int) XoApplication.getXoClient().getDatabase().getInvitedMeRequestsCount();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (mInvitedMeCount > 0) {
+            notificationBadgeTextView.setVisibility(View.VISIBLE);
+        }
+        notificationBadgeTextView.setText(Integer.toString(mInvitedMeCount));
         XoApplication.getXoClient().registerContactListener(mClientContactsAdapter);
+        XoApplication.getXoClient().registerContactListener(this);
     }
 
     @Override
     public void onPause() {
         super.onPause();
         XoApplication.getXoClient().unregisterContactListener(mClientContactsAdapter);
+        XoApplication.getXoClient().unregisterContactListener(this);
     }
 
     @Override
@@ -62,11 +85,55 @@ public class ClientListFragment extends ListFragment implements IPagerFragment {
 
     @Override
     public View getCustomTabView(Context context) {
-        return null;
+        tabView = LayoutInflater.from(context).inflate(R.layout.view_contacts_tab_friends, null);
+        return tabView;
     }
 
     @Override
     public String getTabName(Resources resources) {
         return resources.getString(R.string.contacts_tab_friends);
+    }
+
+    @Override
+    public void onClientRelationshipChanged(TalkClientContact contact) {
+        if (contact.getClientRelationship().invitedMe()) {
+            try {
+                mInvitedMeCount = (int) XoApplication.getXoClient().getDatabase().getInvitedMeRequestsCount();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (mInvitedMeCount > 0) {
+                notificationBadgeTextView.setVisibility(View.VISIBLE);
+                notificationBadgeTextView.setText(Integer.toString(mInvitedMeCount));
+            } else {
+                notificationBadgeTextView.setVisibility(View.GONE);
+                notificationBadgeTextView.setText("");
+            }
+        }
+    }
+
+    @Override
+    public void onContactAdded(TalkClientContact contact) {
+
+    }
+
+    @Override
+    public void onContactRemoved(TalkClientContact contact) {
+
+    }
+
+    @Override
+    public void onClientPresenceChanged(TalkClientContact contact) {
+
+    }
+
+    @Override
+    public void onGroupPresenceChanged(TalkClientContact contact) {
+
+    }
+
+    @Override
+    public void onGroupMembershipChanged(TalkClientContact contact) {
+
     }
 }
