5a6f53b328d8659bb0967a41592733492954f361
Guido Lorenz <guido.lorenz@artcom.de>
2014-10-07 10:35:13 +0200

    Fix whitespace

--- android/src/main/java/com/hoccer/xo/android/activity/ComposableActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/ComposableActivity.java
@@ -26,9 +26,9 @@ public abstract class ComposableActivity extends XoActivity {
      */
     @SuppressWarnings("unchecked")
     public <T extends ActivityComponent> T getComponent(final Class<T> clazz) {
-        for(final ActivityComponent component : mComponents) {
-            if(clazz.equals(component.getClass())) {
-                return (T)component;
+        for (final ActivityComponent component : mComponents) {
+            if (clazz.equals(component.getClass())) {
+                return (T) component;
             }
         }
 
@@ -42,7 +42,7 @@ public abstract class ComposableActivity extends XoActivity {
         // set list of components
         mComponents = createComponents();
 
-        for(final ActivityComponent component : mComponents) {
+        for (final ActivityComponent component : mComponents) {
             component.onCreate(savedInstanceState);
         }
     }
@@ -50,7 +50,7 @@ public abstract class ComposableActivity extends XoActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        for(final ActivityComponent component : mComponents) {
+        for (final ActivityComponent component : mComponents) {
             component.onStart();
         }
     }
@@ -58,7 +58,7 @@ public abstract class ComposableActivity extends XoActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        for(final ActivityComponent component : mComponents) {
+        for (final ActivityComponent component : mComponents) {
             component.onResume();
         }
     }
@@ -71,7 +71,7 @@ public abstract class ComposableActivity extends XoActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         boolean result = super.onCreateOptionsMenu(menu);
 
-        if(result) {
+        if (result) {
             for (final ActivityComponent component : mComponents) {
                 if (!component.onCreateOptionsMenu(menu)) {
                     return false;
@@ -87,7 +87,7 @@ public abstract class ComposableActivity extends XoActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         boolean isConsumed = super.onOptionsItemSelected(item);
 
-        if(!isConsumed) {
+        if (!isConsumed) {
             for (final ActivityComponent component : mComponents) {
                 if (component.onOptionsItemSelected(item)) {
                     return true;
@@ -103,7 +103,7 @@ public abstract class ComposableActivity extends XoActivity {
     protected void onPause() {
         super.onPause();
 
-        for(int i = mComponents.length - 1; i >= 0; i--) {
+        for (int i = mComponents.length - 1; i >= 0; i--) {
             mComponents[i].onPause();
         }
     }
@@ -112,7 +112,7 @@ public abstract class ComposableActivity extends XoActivity {
     protected void onStop() {
         super.onStop();
 
-        for(int i = mComponents.length - 1; i >= 0; i--) {
+        for (int i = mComponents.length - 1; i >= 0; i--) {
             mComponents[i].onStop();
         }
     }
@@ -121,7 +121,7 @@ public abstract class ComposableActivity extends XoActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        for(int i = mComponents.length - 1; i >= 0; i--) {
+        for (int i = mComponents.length - 1; i >= 0; i--) {
             mComponents[i].onDestroy();
         }
     }
