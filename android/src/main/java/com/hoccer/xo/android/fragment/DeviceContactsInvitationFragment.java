hoccer/hoccer-talk-spike

commit e20cc91920e2b9b532648e90330a3f4961b4dfad

Update view more dynamically
* Add avatar image
* Show progress wheel in well box, not as overlay

android/src/main/java/com/hoccer/xo/android/fragment/DeviceContactsInvitationFragment.java
@@ -62,7 +62,7 @@ public class DeviceContactsInvitationFragment extends SearchableListFragment {
         super.onViewCreated(view, savedInstanceState);
 
         mIsSmsInvitation = getActivity().getIntent().getBooleanExtra(EXTRA_IS_SMS_INVITATION, true);
-        mProgressOverlay = (RelativeLayout) view.findViewById(R.id.rl_progress_overlay);
+        mProgressOverlay = (RelativeLayout) view.findViewById(R.id.ll_in_progress);
 
         final Button inviteButton = (Button) view.findViewById(R.id.bt_continue);
         inviteButton.setOnClickListener(new InviteButtonClickListener());

android/res/drawable/rounded_frame.xml
@@ -1,0 +1,5 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<selector xmlns:android="http://schemas.android.com/apk/res/android">
+
+</selector>

android/res/layout/fragment_import_credentials.xml
@@ -1,86 +1,103 @@
 <?xml version="1.0" encoding="utf-8"?>
 
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
-                android:layout_width="match_parent"
-                android:layout_height="match_parent">
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+              android:orientation="vertical"
+              android:layout_width="match_parent"
+              android:layout_height="match_parent"
+              android:layout_marginLeft="10dp"
+              android:layout_marginRight="10dp">
 
-    <LinearLayout android:orientation="vertical"
-                  android:layout_width="match_parent"
-                  android:layout_height="match_parent"
-                  android:layout_marginLeft="10dp"
-                  android:layout_marginRight="10dp">
+    <TextView
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:text="@string/existing_hoccer_xo_profile"
+            style="@style/ParagraphText"/>
 
-        <TextView
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:text="@string/existing_hoccer_xo_profile"
-                style="@style/ParagraphText"/>
+    <RelativeLayout
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:orientation="vertical"
+            android:background="@drawable/well_box">
 
         <LinearLayout
                 android:id="@+id/ll_xo_profile"
                 android:layout_width="match_parent"
                 android:layout_height="wrap_content"
-                android:orientation="vertical"
-                android:background="@drawable/well_box"
+                android:orientation="horizontal"
                 android:visibility="gone">
 
-            <TextView
-                    android:id="@+id/tv_user_name"
-                    android:text="Username"
-                    android:layout_width="wrap_content"
-                    android:layout_height="wrap_content"
-                    android:textAppearance="@android:style/TextAppearance.DeviceDefault.Large"/>
+            <ImageView android:layout_width="wrap_content" android:layout_height="wrap_content"
+                       android:src="@drawable/avatar_default_contact" android:layout_marginRight="10dp"/>
 
-            <TextView
-                    android:id="@+id/tv_contacts_count"
-                    android:text="13 Contacts"
-                    android:layout_width="wrap_content"
-                    android:layout_height="wrap_content"
-                    android:textAppearance="@android:style/TextAppearance.DeviceDefault.Small.Inverse"/>
+            <LinearLayout android:layout_width="wrap_content" android:layout_height="wrap_content"
+                          android:orientation="vertical">
+                <TextView
+                        android:id="@+id/tv_user_name"
+                        android:text="Username"
+                        android:layout_width="wrap_content"
+                        android:layout_height="wrap_content"
+                        android:textAppearance="@android:style/TextAppearance.DeviceDefault.Large"/>
+
+                <TextView
+                        android:id="@+id/tv_contacts_count"
+                        android:text="13 Contacts"
+                        android:layout_width="wrap_content"
+                        android:layout_height="wrap_content"
+                        android:textAppearance="@android:style/TextAppearance.DeviceDefault.Small.Inverse"/>
+            </LinearLayout>
 
         </LinearLayout>
-
-        <TextView
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:text="@string/import_or_create_new_profile"
-                style="@style/ParagraphText"/>
 
         <LinearLayout
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:orientation="horizontal">
-
-            <Button
-                    android:id="@+id/btn_import_credentials"
-                    android:layout_width="0dp"
-                    android:layout_height="wrap_content"
-                    android:layout_weight="1"
-                    android:text="@string/import_credentials"/>
-
-            <Button
-                    android:id="@+id/btn_create_new_client"
-                    android:layout_width="0dp"
-                    android:layout_height="wrap_content"
-                    android:layout_weight="1"
-                    android:text="@string/new_profile"
-                    style="@style/ButtonGrayAppBaseTheme"/>
-        </LinearLayout>
-
-    </LinearLayout>
-
-    <RelativeLayout
-            android:id="@+id/rl_progress_overlay"
-            android:layout_width="match_parent"
-            android:layout_height="match_parent"
-            android:background="@color/xo_black_transparency_50_percent"
-            android:clickable="true"
-            android:visibility="gone">
-
-        <ProgressBar
+                android:id="@+id/ll_in_progress"
                 android:layout_width="wrap_content"
                 android:layout_height="wrap_content"
-                android:layout_centerInParent="true"/>
+                android:visibility="visible">
+
+            <ProgressBar
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:layout_marginRight="10dp"/>
+
+            <TextView
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:text="@string/reading_profile_data"
+                    android:textAppearance="@android:style/TextAppearance.DeviceDefault.Medium"
+                    android:layout_gravity="center_vertical"/>
+        </LinearLayout>
+
     </RelativeLayout>
 
-</RelativeLayout>
+    <TextView
+            android:id="@+id/tv_import_profile"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:text="@string/import_or_create_new_profile"
+            style="@style/ParagraphText"
+            android:visibility="gone"/>
+
+    <LinearLayout
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:orientation="horizontal">
+
+        <Button
+                android:id="@+id/btn_import_credentials"
+                android:layout_width="0dp"
+                android:layout_height="wrap_content"
+                android:layout_weight="1"
+                android:text="@string/import_credentials"
+                android:visibility="gone"/>
+
+        <Button
+                android:id="@+id/btn_create_new_client"
+                android:layout_width="0dp"
+                android:layout_height="wrap_content"
+                android:layout_weight="1"
+                android:text="@string/new_profile"
+                android:visibility="gone"
+                style="@style/ButtonGrayAppBaseTheme"/>
+    </LinearLayout>
+
+</LinearLayout>

android/res/values-de/strings.xml
@@ -430,7 +430,7 @@
     <string name="import_or_create_new_profile">Du kannst dein bestehendes Profil weiterverwenden oder ein neues erstellen.</string>
     <string name="import_credentials">Profil importieren</string>
     <string name="new_profile">Neues Profil</string>
-
+    <string name="reading_profile_data">Reading profile data ..</string>
 
     <plurals name="message_count">
         <item quantity="one">1 Nachricht</item>

android/res/values/strings.xml
@@ -454,8 +454,9 @@
     <string name="import_or_create_new_profile">You can import your old Profile or create a fresh new one.</string>
     <string name="import_credentials">Import Credentials</string>
     <string name="new_profile">New Profile</string>
+    <string name="reading_profile_data">Reading profile data ..</string>
 
-   <plurals name="message_count">
+    <plurals name="message_count">
         <item quantity="one">1 Message</item>
         <item quantity="other">%d Messages</item>
     </plurals>

android/src/main/java/com/hoccer/xo/android/fragment/ImportCredentialFragment.java
@@ -24,10 +24,14 @@ public class ImportCredentialFragment extends XoFragment {
     private static final Logger LOG = Logger.getLogger(ImportCredentialFragment.class);
     private RegistrationActivity mRegistrationActivity;
     private Credentials mCredentials;
-    private View mProgressOverlay;
+    private View mProgressLayout;
     private TextView mUserNameTextView;
     private TextView mContactsCountTextView;
     private LinearLayout mXoProfileLayout;
+    private TextView mImportProfileTextView;
+    private Button mImportButton;
+    private Button mNewClientButton;
+    private Integer mContactCount;
 
 
     @Override
@@ -45,17 +49,17 @@ public class ImportCredentialFragment extends XoFragment {
     public void onViewCreated(final View view, final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mProgressOverlay = view.findViewById(R.id.rl_progress_overlay);
+        mProgressLayout = view.findViewById(R.id.ll_in_progress);
         mUserNameTextView = (TextView) view.findViewById(R.id.tv_user_name);
         mContactsCountTextView = (TextView) view.findViewById(R.id.tv_contacts_count);
         mXoProfileLayout = (LinearLayout) view.findViewById(R.id.ll_xo_profile);
-
-        mProgressOverlay.setVisibility(View.VISIBLE);
+        mImportProfileTextView = (TextView) view.findViewById(R.id.tv_import_profile);
+        mImportButton = (Button) view.findViewById(R.id.btn_import_credentials);
+        mNewClientButton = (Button) getView().findViewById(R.id.btn_create_new_client);
 
         importCredentials();
 
-        final Button importButton = (Button) view.findViewById(R.id.btn_import_credentials);
-        importButton.setOnClickListener(new View.OnClickListener() {
+        mImportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
 
@@ -67,8 +71,7 @@ public class ImportCredentialFragment extends XoFragment {
             }
         });
 
-        final Button newClientButton = (Button) getView().findViewById(R.id.btn_create_new_client);
-        newClientButton.setOnClickListener(new View.OnClickListener() {
+        mNewClientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
 
@@ -84,17 +87,15 @@ public class ImportCredentialFragment extends XoFragment {
         CredentialImporter.importCredentials(mRegistrationActivity, new CredentialImporter.CredentialImportListener() {
             @Override
             public void onSuccess(final Credentials credentials, final Integer contactCount) {
-                
+
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
 
                         LOG.info("Credentials import succeeded");
                         mCredentials = credentials;
-                        mUserNameTextView.setText(mCredentials.getClientName());
-                        mContactsCountTextView.setText(contactCount.toString());
-                        mXoProfileLayout.setVisibility(View.VISIBLE);
-                        mProgressOverlay.setVisibility(View.GONE);
+                        mContactCount = contactCount;
+                        updateView();
                     }
                 });
             }
@@ -108,10 +109,20 @@ public class ImportCredentialFragment extends XoFragment {
 
                         LOG.error("Credentials import failed");
                         mRegistrationActivity.startNewClientRegistration();
-                        mProgressOverlay.setVisibility(View.GONE);
+                        mProgressLayout.setVisibility(View.GONE);
                     }
                 });
             }
         });
+    }
+
+    private void updateView() {
+        mUserNameTextView.setText(mCredentials.getClientName());
+        mContactsCountTextView.setText(mContactCount.toString());
+        mProgressLayout.setVisibility(View.GONE);
+        mXoProfileLayout.setVisibility(View.VISIBLE);
+        mImportProfileTextView.setVisibility(View.VISIBLE);
+        mImportButton.setVisibility(View.VISIBLE);
+        mNewClientButton.setVisibility(View.VISIBLE);
     }
 }

commit e9901f6e4e13238b7eeffdfc687aced8c5d5747c

Fix SMS/E-Mail invitation
Broke in e20cc91920e2b9b532648e90330a3f4961b4dfad.
Fixes #567.

android/src/main/java/com/hoccer/xo/android/fragment/DeviceContactsInvitationFragment.java
@@ -62,7 +62,7 @@ public class DeviceContactsInvitationFragment extends SearchableListFragment {
         super.onViewCreated(view, savedInstanceState);
 
         mIsSmsInvitation = getActivity().getIntent().getBooleanExtra(EXTRA_IS_SMS_INVITATION, true);
-        mProgressOverlay = (RelativeLayout) view.findViewById(R.id.ll_in_progress);
+        mProgressOverlay = (RelativeLayout) view.findViewById(R.id.rl_progress_overlay);
 
         final Button inviteButton = (Button) view.findViewById(R.id.bt_continue);
         inviteButton.setOnClickListener(new InviteButtonClickListener());