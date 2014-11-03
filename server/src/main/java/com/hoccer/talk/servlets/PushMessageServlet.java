eabb738ead6cf9a58633d3cc87cae731fdda1a58
Guido Lorenz <guido.lorenz@artcom.de>
2014-11-03 11:31:21 +0100

    Send push messages as defined in HTTP POST request JSON payload

--- server/src/main/java/com/hoccer/talk/servlets/PushMessageServlet.java
+++ server/src/main/java/com/hoccer/talk/servlets/PushMessageServlet.java
@@ -1,6 +1,9 @@
 package com.hoccer.talk.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.hoccer.talk.model.TalkClient;
+import com.hoccer.talk.model.TalkClientHostInfo;
 import com.hoccer.talk.server.ITalkServerDatabase;
 import com.hoccer.talk.server.TalkServer;
 
@@ -9,25 +12,56 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class PushMessageServlet extends HttpServlet {
 
     private TalkServer mServer;
+    private ObjectMapper mObjectMapper;
+    private ITalkServerDatabase mDatabase;
 
     @Override
     public void init() throws ServletException {
         mServer = (TalkServer) getServletContext().getAttribute("server");
+        mDatabase = mServer.getDatabase();
+        mObjectMapper = new ObjectMapper();
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ITalkServerDatabase database = mServer.getDatabase();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            CollectionType type = mObjectMapper.getTypeFactory().constructCollectionType(List.class, PushMessageJson.class);
+            List<PushMessageJson> messages = mObjectMapper.readValue(request.getReader(), type);
 
-        for (TalkClient client : database.findAllClients()) {
-            mServer.getPushAgent().submitSystemMessage(client, "good day, sir!");
+            for (PushMessageJson message : messages) {
+                submitPushMessage(mDatabase, message);
+            }
+
+            response.setStatus(200);
+        } catch (Exception e) {
+            response.getWriter().println(e.getMessage());
+            response.setStatus(400);
         }
+    }
 
-        response.setStatus(200);
-        response.getWriter().println("good");
+    private void submitPushMessage(ITalkServerDatabase database, PushMessageJson message) {
+        for (TalkClient client : database.findAllClients()) {
+            TalkClientHostInfo info = database.findClientHostInfoForClient(client.getClientId());
+
+            if (shouldClientReceiveMessage(info, message)) {
+                mServer.getPushAgent().submitSystemMessage(client, message.message);
+            }
+        }
+    }
+
+    private boolean shouldClientReceiveMessage(TalkClientHostInfo hostInfo, PushMessageJson message) {
+        return hostInfo.getClientLanguage().equals(message.language) &&
+                hostInfo.getClientName().equals(message.clientName);
+    }
+
+    public static class PushMessageJson {
+        public String language;
+        public String clientName;
+        public String message;
     }
 }
