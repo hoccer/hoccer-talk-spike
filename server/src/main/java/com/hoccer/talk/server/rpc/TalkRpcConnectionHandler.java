a45191a4a07be02ecedcc7f11d3f6b569bb8b0b7
Guido Lorenz <guido.lorenz@artcom.de>
2014-11-05 18:10:02 +0100

    Add new server protocol V5, support both V4 and V5
    Fixes #553.

--- server/src/main/java/com/hoccer/talk/server/rpc/TalkRpcConnectionHandler.java
+++ server/src/main/java/com/hoccer/talk/server/rpc/TalkRpcConnectionHandler.java
@@ -40,10 +40,16 @@ public class TalkRpcConnectionHandler extends WebSocketHandler {
     public static final String TALK_TEXT_PROTOCOL_NAME_V4 = "com.hoccer.talk.v4";
     public static final String TALK_BINARY_PROTOCOL_NAME_V4 = "com.hoccer.talk.v4.bson";
 
+    // Version 5
+    public static final String TALK_TEXT_PROTOCOL_NAME_V5 = "com.hoccer.talk.v5";
+    public static final String TALK_BINARY_PROTOCOL_NAME_V5 = "com.hoccer.talk.v5.bson";
+
     public static List<String> getCurrentProtocolVersions() {
         List<String> list = new ArrayList<String>();
         list.add(TALK_BINARY_PROTOCOL_NAME_V4);
         list.add(TALK_TEXT_PROTOCOL_NAME_V4);
+        list.add(TALK_BINARY_PROTOCOL_NAME_V5);
+        list.add(TALK_TEXT_PROTOCOL_NAME_V5);
         return list;
     }
 
@@ -76,9 +82,11 @@ public class TalkRpcConnectionHandler extends WebSocketHandler {
      */
     @Override
     public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
-        if (TALK_TEXT_PROTOCOL_NAME_V4.equals(protocol)) {
+        if (TALK_TEXT_PROTOCOL_NAME_V4.equals(protocol) ||
+                TALK_TEXT_PROTOCOL_NAME_V5.equals(protocol)) {
             return createTalkActiveConnection(request, mTalkServer.getJsonMapper(), false);
-        } else if (TALK_BINARY_PROTOCOL_NAME_V4.equals(protocol)) {
+        } else if (TALK_BINARY_PROTOCOL_NAME_V4.equals(protocol) ||
+                TALK_BINARY_PROTOCOL_NAME_V5.equals(protocol)) {
             return createTalkActiveConnection(request, mTalkServer.getBsonMapper(), true);
         } else if (TALK_TEXT_PROTOCOL_NAME_V1.equals(protocol) ||
                 TALK_TEXT_PROTOCOL_NAME_V2.equals(protocol) ||
