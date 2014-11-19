89bc9ef06cc1f464a2250f10664eefe59654e4f5
Guido Lorenz <guido.lorenz@artcom.de>
2014-11-19 14:33:24 +0100

    Send GCM push messages in badges of 1000
    The GCM only accepts up to 1000 recipients at a time, so we need to send the
    message in badges. While the documentation said the library would take care
    of this limitation, is actually doesn't. At least not the one we are using.

--- server/src/main/java/com/hoccer/talk/server/push/GcmPushProvider.java
+++ server/src/main/java/com/hoccer/talk/server/push/GcmPushProvider.java
@@ -5,13 +5,16 @@ import com.google.android.gcm.server.MulticastResult;
 import com.google.android.gcm.server.Result;
 import com.google.android.gcm.server.Sender;
 import com.hoccer.talk.model.TalkClient;
-import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.map.MultiValueMap;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 
 public class GcmPushProvider extends PushProvider {
     private static final Logger LOG = Logger.getLogger(GcmPushProvider.class);
@@ -43,61 +46,81 @@ public class GcmPushProvider extends PushProvider {
         }
 
         for (String gcmPackage : map.keySet()) {
-            Collection<String> registrations = map.getCollection(gcmPackage);
-            Collection<String> uniqueRegistrations = getUniqueRegistrations(gcmPackage, registrations);
+            try {
+                Collection<String> registrations = map.getCollection(gcmPackage);
+                Collection<String> uniqueRegistrations = getUniqueRegistrations(gcmPackage, registrations);
 
-            LOG.info("sending message to " + uniqueRegistrations.size() + " unique GCM registrations");
+                LOG.info("sending message to " + uniqueRegistrations.size() + " unique GCM registrations");
 
-            doPushMessage(gcmPackage, uniqueRegistrations, message);
+                doPushMessage(gcmPackage, uniqueRegistrations, message);
+            } catch (IOException e) {
+                LOG.error("Failed to send push message for package " + gcmPackage, e);
+            }
         }
     }
 
-    private Collection<String> getUniqueRegistrations(String gcmPackage, Collection<String> registrations) {
+    private Collection<String> getUniqueRegistrations(String gcmPackage, Collection<String> registrations) throws IOException {
         Message gcmMessage = new Message.Builder()
                 .restrictedPackageName(gcmPackage)
                 .dryRun(true)
                 .build();
 
-        try {
-            ArrayList<String> registrationsList = new ArrayList<String>(registrations);
-            MulticastResult multicastResult = mSender.send(gcmMessage, registrationsList, 10);
-            List<Result> results = multicastResult.getResults();
-            HashSet<String> uniqueRegistrations = new HashSet<String>();
+        ArrayList<String> registrationsList = new ArrayList<String>(registrations);
+        List<Result> results = send(registrationsList, gcmMessage);
+        HashSet<String> uniqueRegistrations = new HashSet<String>();
 
-            for (int i = 0; i < results.size(); i++) {
-                if (results.get(i).getCanonicalRegistrationId() != null) {
-                    uniqueRegistrations.add(results.get(i).getCanonicalRegistrationId());
-                } else {
-                    uniqueRegistrations.add(registrationsList.get(i));
-                }
+        for (int i = 0; i < results.size(); i++) {
+            if (results.get(i).getCanonicalRegistrationId() != null) {
+                uniqueRegistrations.add(results.get(i).getCanonicalRegistrationId());
+            } else {
+                uniqueRegistrations.add(registrationsList.get(i));
             }
-
-            return uniqueRegistrations;
-        } catch (IOException e) {
-            LOG.error("GCM multicast error", e);
         }
 
-        return CollectionUtils.emptyCollection();
+        return uniqueRegistrations;
     }
 
-    private void doPushMessage(String gcmPackage, Collection<String> uniqueGcmRegistrations, String message) {
+    private void doPushMessage(String gcmPackage, Collection<String> uniqueGcmRegistrations, String message) throws IOException {
         Message gcmMessage = new Message.Builder()
                 .restrictedPackageName(gcmPackage)
                 .addData("message", message)
                 .build();
 
-        try {
-            MulticastResult res = mSender.send(gcmMessage, new ArrayList<String>(uniqueGcmRegistrations), 10);
+        List<Result> results = send(uniqueGcmRegistrations, gcmMessage);
 
-            if (res.getFailure() > 0) {
-                LOG.error("GCM multicast returned " + res.getFailure() + " errors");
-            }
+        int failures = 0;
+        int canonicalIds = 0;
 
-            if (res.getCanonicalIds() > 0) {
-                LOG.info("GCM multicast returned " + res.getCanonicalIds() + " canonical ids");
+        for (Result result : results) {
+            if (result.getMessageId() != null) {
+                if (result.getCanonicalRegistrationId() != null) {
+                    canonicalIds += 1;
+                }
+            } else {
+                failures += 1;
             }
-        } catch (IOException e) {
-            LOG.error("GCM multicast error", e);
         }
+
+        LOG.error("GCM multicast returned " + failures + " failures");
+        LOG.info("GCM multicast returned " + canonicalIds + " canonical ids");
+    }
+
+    private List<Result> send(Collection<String> registrations, Message gcmMessage) throws IOException {
+        List<List<String>> badgedRegistrations = ListUtils.partition(new ArrayList<String>(registrations), 1000);
+        ArrayList<Result> results = new ArrayList<Result>();
+
+        LOG.debug("Sending push messages in " + badgedRegistrations.size() + " badge(s)");
+
+        for (List<String> badge : badgedRegistrations) {
+            MulticastResult badgeResult = mSender.send(gcmMessage, badge, 10);
+            results.addAll(badgeResult.getResults());
+
+            LOG.debug("Sent badge (" +
+                    badgeResult.getSuccess() + " success, " +
+                    badgeResult.getFailure() + " failure, " +
+                    badgeResult.getCanonicalIds() + " canonical id(s))");
+        }
+
+        return results;
     }
 }
