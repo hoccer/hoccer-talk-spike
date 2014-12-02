dfa0a311b7073af5691a7cb583814cf19aa2a2a6
Mirko Geissler <mirko.geissler@artcom.de>
2014-12-02 22:44:02 +0100

    Cleanup MultiImageSelector

--- android/src/main/java/com/hoccer/xo/android/content/MultiImageSelector.java
+++ android/src/main/java/com/hoccer/xo/android/content/MultiImageSelector.java
@@ -4,14 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import com.artcom.hoccer.R;
 import com.hoccer.talk.content.IContentObject;
 import com.hoccer.xo.android.activity.MultiImagePickerActivity;
 import com.hoccer.xo.android.content.contentselectors.IContentCreator;
 import com.hoccer.xo.android.content.contentselectors.ImageSelector;
 import com.hoccer.xo.android.util.ColorSchemeManager;
-import com.artcom.hoccer.R;
 import org.apache.log4j.Logger;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 
@@ -40,21 +39,17 @@ public class MultiImageSelector extends ImageSelector {
     @Override
     public boolean isValidIntent(Context context, Intent intent) {
         String[] arrayExtra = intent.getStringArrayExtra(MultiImagePickerActivity.EXTRA_IMAGES);
-        if(arrayExtra != null && arrayExtra.length > 0) {
-            return true;
-        }
-        return false;
+        return arrayExtra != null && arrayExtra.length > 0;
     }
 
     @Override
     public Intent createSelectionIntent(Context context) {
-        Intent intent = new Intent(context, MultiImagePickerActivity.class);
-        return intent;
+        return new Intent(context, MultiImagePickerActivity.class);
     }
 
     public ArrayList<IContentObject> createObjectsFromSelectionResult(Context context, Intent intent) {
         ArrayList<IContentObject> result = new ArrayList<IContentObject>();
-        if(!isValidIntent(context, intent)) {
+        if (!isValidIntent(context, intent)) {
             return result;
         }
 
