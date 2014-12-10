10c5bafda89bf406c2f9dff4f988539fe602c5ed
Jens Eraßmy <jens.erassmy@artcom.de>
2014-12-10 17:54:28 +0100

    Refactor broadcast and notification handling

--- android/src/main/java/com/hoccer/xo/android/backup/BackupService.java
+++ android/src/main/java/com/hoccer/xo/android/backup/BackupService.java
@@ -1,9 +1,9 @@
 package com.hoccer.xo.android.backup;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.*;
@@ -26,6 +26,7 @@ public class BackupService extends Service {
     private LocalBroadcastManager mLocalBroadcastManager;
     private Looper mLooper;
     private ServiceHandler mServiceHandler;
+    private NotificationManager mNotificationManager;
 
     private final class ServiceHandler extends Handler {
 
@@ -57,61 +58,80 @@ public class BackupService extends Service {
     public void onCreate() {
         HandlerThread thread = new HandlerThread(BACKUP_HANDLER_THREAD_NAME, Process.THREAD_PRIORITY_FOREGROUND);
         thread.start();
-
         mLooper = thread.getLooper();
         mServiceHandler = new ServiceHandler(mLooper);
 
         mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        final Bundle extras = intent.getExtras();
-
-        Message msg = mServiceHandler.obtainMessage();
-        msg.arg1 = startId;
-        msg.setData(extras);
-        mServiceHandler.sendMessage(msg);
+        if (intent != null) {
+            final Bundle extras = intent.getExtras();
+            Message msg = mServiceHandler.obtainMessage();
+            msg.arg1 = startId;
+            msg.setData(extras);
+            mServiceHandler.sendMessage(msg);
+        }
 
         return START_NOT_STICKY; //TODO choose right return value
     }
 
     public void createBackup(final String password, String type) {
-        triggerBackupCreationInProgressNotification();
 
         try {
-            Backup backup;
+            startInForeground();
+
+            Backup backup = null;
             if (type.equals(BackupType.COMPLETE.toString())) {
                 backup = BackupFactory.createCompleteBackup(password);
             } else if (type.equals(BackupType.DATABASE.toString())) {
                 backup = BackupFactory.createDatabaseBackup(password);
             }
-            broadcastBackupSucceeded();
-            triggerBackupCreationSucceededNotification();
-        } catch (InterruptedException e) {
-            broadcastBackupCanceled();
+
             stopForeground(true);
+
+            triggerSuccessNotification();
+
+            broadcastResult(backup);
+        } catch (InterruptedException e) {
+            broadcastCanceled();
             e.printStackTrace();
         } catch (Exception e) {
-            broadcastBackupFailed();
-            triggerBackupCreationFailedNotification();
+            broadcastFailed();
+            triggerFailedNotification();
             e.printStackTrace();
         } finally {
+            stopForeground(true); // TODO Check for exceptions?
             stopSelf();
         }
     }
 
-    private void broadcastBackupSucceeded() {
+    private void startInForeground() {
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Creating backup ..")
+                .setOngoing(true)
+                .setContentIntent(createPendingIntent())
+                .setPriority(Notification.PRIORITY_MAX).build();
+
+        startForeground(BACKUP_NOTIFICATION_ID, notification);
+    }
+
+    private void broadcastResult(Backup backup) {
         Intent intent = new Intent(IntentHelper.ACTION_BACKUP_SUCCEEDED);
+        intent.putExtra("result", backup);
         mLocalBroadcastManager.sendBroadcast(intent);
     }
 
-    private void broadcastBackupCanceled() {
+    private void broadcastCanceled() {
         Intent intent = new Intent(IntentHelper.ACTION_BACKUP_CANCELED);
         mLocalBroadcastManager.sendBroadcast(intent);
     }
 
-    private void broadcastBackupFailed() {
+    private void broadcastFailed() {
         Intent intent = new Intent(IntentHelper.ACTION_BACKUP_FAILED);
         mLocalBroadcastManager.sendBroadcast(intent);
     }
@@ -120,55 +140,32 @@ public class BackupService extends Service {
         mLooper.getThread().interrupt();
     }
 
-    private void triggerBackupCreationInProgressNotification() {
-        Intent resultIntent = new Intent(this, XoPreferenceActivity.class);
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this)
-                .addNextIntent(resultIntent);
-
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setContentTitle("Creating backup ..")
-                .setOngoing(true)
-                .setContentIntent(resultPendingIntent)
-                .setPriority(Notification.PRIORITY_MAX);
-
-        startForeground(BACKUP_NOTIFICATION_ID, builder.build());
-    }
-
-    private void triggerBackupCreationSucceededNotification() {
-        Intent resultIntent = new Intent(this, XoPreferenceActivity.class);
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this)
-                .addNextIntent(resultIntent);
-
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+    private void triggerSuccessNotification() {
+        Notification notification = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle("Successfully created backup.")
-                .setContentIntent(resultPendingIntent)
-                .setPriority(Notification.PRIORITY_MAX);
+                .setContentIntent(createPendingIntent())
+                .setPriority(Notification.PRIORITY_MAX).build();
 
-        startForeground(BACKUP_NOTIFICATION_ID, builder.build());
+        mNotificationManager.notify(BACKUP_NOTIFICATION_ID, notification);
     }
 
-    private void triggerBackupCreationFailedNotification() {
+    private void triggerFailedNotification() {
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Failed creating backup.")
+                .setContentIntent(createPendingIntent())
+                .setPriority(Notification.PRIORITY_MAX).build();
+
+        mNotificationManager.notify(BACKUP_NOTIFICATION_ID, notification);
+    }
+
+    private PendingIntent createPendingIntent() {
         Intent resultIntent = new Intent(this, XoPreferenceActivity.class);
+        resultIntent.putExtra("show_backup_preferences", true);
 
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this)
                 .addNextIntent(resultIntent);
-
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setContentTitle("Failed creating backup.")
-                .setContentIntent(resultPendingIntent)
-                .setPriority(Notification.PRIORITY_MAX);
-
-        startForeground(BACKUP_NOTIFICATION_ID, builder.build());
+        return stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
