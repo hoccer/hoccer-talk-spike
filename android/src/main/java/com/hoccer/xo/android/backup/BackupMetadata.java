9ebd57a99ad0241ed819e30cb558a36a5e6019c1
Jens Eraßmy <jens.erassmy@artcom.de>
2014-12-12 12:53:00 +0100

    Fix BackupMetadata parcelable

--- android/src/main/java/com/hoccer/xo/android/backup/BackupMetadata.java
+++ android/src/main/java/com/hoccer/xo/android/backup/BackupMetadata.java
@@ -2,8 +2,10 @@ package com.hoccer.xo.android.backup;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.File;
 import java.util.Date;
 
 public class BackupMetadata implements Parcelable {
@@ -17,7 +19,7 @@ public class BackupMetadata implements Parcelable {
     @JsonProperty("creationDate")
     private Date mCreationDate;
 
-    public BackupMetadata() {}
+    public BackupMetadata(){}
 
     public BackupMetadata(BackupType type, String clientName, Date creationDate) {
         mBackupType = type;
@@ -36,6 +38,24 @@ public class BackupMetadata implements Parcelable {
     public Date getCreationDate() {
         return mCreationDate;
     }
+
+    private BackupMetadata(Parcel source) {
+        mBackupType = BackupType.valueOf(source.readString());
+        mClientName = source.readString();
+        mCreationDate = new Date(source.readLong());
+    }
+
+    public static final Creator<? extends BackupMetadata> CREATOR = new Creator<BackupMetadata>() {
+        @Override
+        public BackupMetadata createFromParcel(Parcel source) {
+            return new BackupMetadata(source);
+        }
+
+        @Override
+        public BackupMetadata[] newArray(int size) {
+            return new BackupMetadata[size];
+        }
+    };
 
     @Override
     public int describeContents() {
