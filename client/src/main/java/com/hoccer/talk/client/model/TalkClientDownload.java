d1a28a81d48b9e758497ed8081bc93af7d2cc86f
Jens Eraßmy <jens.erassmy@artcom.de>
2015-01-09 17:58:09 +0100

    Store relative file path in dataFile field

--- client/src/main/java/com/hoccer/talk/client/model/TalkClientDownload.java
+++ client/src/main/java/com/hoccer/talk/client/model/TalkClientDownload.java
@@ -645,7 +645,7 @@ public class TalkClientDownload extends XoTransfer implements IXoTransferObject 
                         File newName = new File(destinationPath);
                         if (destination.renameTo(newName)) {
                             this.decryptedFile = destinationFileName;
-                            this.dataFile = destinationPath;
+                            this.dataFile = destinationFileName;
                         } else {
                             LOG.warn("could not rename file");
                         }
