b0e190d4a3c7327761b7c236b83cfabc78c475ed
Mirko Geissler <mirko.geissler@artcom.de>
2015-01-15 14:44:28 +0100

    Convert URI to filePath on upload

--- android/src/main/java/com/hoccer/xo/android/content/SelectedContent.java
+++ android/src/main/java/com/hoccer/xo/android/content/SelectedContent.java
@@ -231,11 +231,12 @@ public class SelectedContent implements IContentObject {
             contentUrl = null;
         }
 
+        String filePath = Uri.parse(object.getContentDataUrl()).getPath();
         TalkClientUpload upload = new TalkClientUpload();
         upload.initializeAsAttachment(
                 object.getFileName(),
                 contentUrl,
-                object.getContentDataUrl(),
+                filePath,
                 object.getContentType(),
                 object.getContentMediaType(),
                 object.getContentAspectRatio(),
