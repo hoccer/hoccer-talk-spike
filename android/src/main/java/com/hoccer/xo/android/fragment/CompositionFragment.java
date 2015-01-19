9f4bf2e197d97fcdf6b403790f8e790acf9f838b
Mirko Geissler <mirko.geissler@artcom.de>
2015-01-19 09:51:10 +0100

    Fix path when querying content data path

--- android/src/main/java/com/hoccer/xo/android/fragment/CompositionFragment.java
+++ android/src/main/java/com/hoccer/xo/android/fragment/CompositionFragment.java
@@ -448,7 +448,7 @@ public class CompositionFragment extends XoFragment implements MotionGestureList
 
     private IContentObject compressImageAttachment(IContentObject contentObject) {
         IContentObject result = null;
-        Uri fileUri = Uri.parse(contentObject.getContentDataUrl());
+        Uri fileUri = UriUtils.getAbsoluteFileUri(contentObject.getContentDataUrl());
         final File imageFile = new File(fileUri.getPath());
         final File compressedImageFile = new File(XoApplication.getCacheStorage(), imageFile.getName());
 
