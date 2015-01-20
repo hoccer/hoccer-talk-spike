f5398189a8651469ec7cf34c6834b026711e0fc3
Jens Eraßmy <jens.erassmy@artcom.de>
2015-01-20 10:35:24 +0100

    Remove development preference and handling for nearby

--- android/src/main/java/com/hoccer/xo/android/activity/ChatsActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/ChatsActivity.java
@@ -43,20 +43,9 @@ public class ChatsActivity extends ComposableActivity implements IXoStateListene
 
     private ViewPager mViewPager;
 
-    private boolean mEnvironmentUpdatesEnabled;
     private boolean mNoUserInput = false;
     private String mPairingToken;
     private ContactsMenuItemActionProvider mContactsMenuItemActionProvider;
-
-    private SharedPreferences.OnSharedPreferenceChangeListener mPreferencesListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            if (key.equals("preference_environment_update")) {
-                mEnvironmentUpdatesEnabled = sharedPreferences.getBoolean("preference_environment_update", true);
-                refreshEnvironmentUpdater(false);
-            }
-        }
-    };
 
     @Override
     protected ActivityComponent[] createComponents() {
@@ -79,7 +68,6 @@ public class ChatsActivity extends ComposableActivity implements IXoStateListene
 
         initViewPager();
         initActionBar();
-        determineRegistrationForEnvironmentUpdates();
 
         handleIntent(getIntent());
     }
@@ -170,12 +158,6 @@ public class ChatsActivity extends ComposableActivity implements IXoStateListene
         }
 
         mContactsMenuItemActionProvider = new ContactsMenuItemActionProvider(this);
-    }
-
-    private void determineRegistrationForEnvironmentUpdates() {
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        pref.registerOnSharedPreferenceChangeListener(mPreferencesListener);
-        mEnvironmentUpdatesEnabled = pref.getBoolean("preference_environment_update", true);
     }
 
     private void showProfileIfClientIsNotRegistered() {
@@ -289,12 +271,10 @@ public class ChatsActivity extends ComposableActivity implements IXoStateListene
         LOG.debug("refreshEnvironmentUpdater");
         Fragment fragment = getFragmentAt(mViewPager.getCurrentItem());
         if (fragment instanceof NearbyChatListFragment) {
-            if (mEnvironmentUpdatesEnabled) {
-                if (isLocationServiceEnabled()) {
-                    LOG.debug("refreshEnvironmentUpdater:startNearbySession");
-                    XoApplication.startNearbySession(force);
-                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                }
+            if (isLocationServiceEnabled()) {
+                LOG.debug("refreshEnvironmentUpdater:startNearbySession");
+                XoApplication.startNearbySession(force);
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             }
         } else {
             shutDownNearbySession();
