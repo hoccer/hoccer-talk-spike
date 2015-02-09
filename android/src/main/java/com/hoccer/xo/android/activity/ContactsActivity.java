173ec3edae8de7bfc04973ee033bafaca074960b
Jens Eraßmy <jens.erassmy@artcom.de>
2015-02-09 16:07:48 +0100

    Handle share intents in ContactsActivity

--- android/src/main/java/com/hoccer/xo/android/activity/ContactsActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/ContactsActivity.java
@@ -1,12 +1,20 @@
 package com.hoccer.xo.android.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.artcom.hoccer.R;
+import com.hoccer.talk.content.SelectedContent;
 import com.hoccer.xo.android.activity.component.ActivityComponent;
 import com.hoccer.xo.android.activity.component.MediaPlayerActivityComponent;
 import com.hoccer.xo.android.activity.component.ViewPagerActivityComponent;
+import com.hoccer.xo.android.content.Clipboard;
+import com.hoccer.xo.android.content.selector.IContentSelector;
+import com.hoccer.xo.android.content.selector.ImageSelector;
+import com.hoccer.xo.android.content.selector.VideoSelector;
 import com.hoccer.xo.android.fragment.ClientContactListFragment;
 import com.hoccer.xo.android.fragment.GroupContactListFragment;
-import com.artcom.hoccer.R;
 
 public class ContactsActivity extends ComposableActivity {
 
@@ -34,6 +42,46 @@ public class ContactsActivity extends ComposableActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Intent.ACTION_SEND.equals(getIntent().getAction())) {
+            handleShareIntent(getIntent());
+        }
+
         enableUpNavigation();
+    }
+    private void handleShareIntent(Intent intent) {
+        Uri contentUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        SelectedContent content = getContent(contentUri, intent.getType());
+        addSharedContentToClipboard(content);
+    }
+
+    private SelectedContent getContent(Uri contentUri, String type) {
+        IContentSelector selector = determineContentSelectorForType(type);
+
+        Intent intent = new Intent();
+        intent.setData(contentUri);
+
+        return selector.createObjectFromSelectionResult(this, intent);
+    }
+
+    private IContentSelector determineContentSelectorForType(String type) {
+        IContentSelector selector = null;
+        if (type.startsWith("image/")) {
+            selector = new ImageSelector(this);
+        } else if (type.startsWith("video/")) {
+            selector = new VideoSelector(this);
+        }
+
+        return selector;
+    }
+
+    private void addSharedContentToClipboard(SelectedContent content) {
+        if (content != null) {
+            Clipboard.getInstance().setContent(content);
+            Toast.makeText(this, getString(R.string.toast_stored_file_in_clipboard), Toast.LENGTH_LONG).show();
+        } else {
+            Clipboard.getInstance().clearContent();
+            Toast.makeText(this, R.string.toast_failed_to_store_file_in_clipboard, Toast.LENGTH_LONG).show();
+        }
     }
 }
