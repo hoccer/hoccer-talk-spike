bbcd03a1f4013f6e1cd4a5544799e43b9073aad2
Mirko Geissler <mirko.geissler@artcom.de>
2015-02-27 12:08:32 +0100

    Use color and string references

--- android/src/main/java/com/hoccer/xo/android/view/ConnectionStateView.java
+++ android/src/main/java/com/hoccer/xo/android/view/ConnectionStateView.java
@@ -50,25 +50,24 @@ public class ConnectionStateView extends LinearLayout implements IXoStateListene
         switch (connectionState) {
             case XoClient.STATE_DISCONNECTED:
                 this.setVisibility(View.VISIBLE);
-                mConnectionStateTextView.setText("disconnected");
+                mConnectionStateTextView.setText(R.string.connection_state_disconnected);
                 break;
             case XoClient.STATE_CONNECTING:
                 this.setVisibility(View.VISIBLE);
-                mConnectionStateTextView.setText("disconnected");
-                break;
-            case XoClient.STATE_REGISTERING:
-                this.setVisibility(View.VISIBLE);
-                mConnectionStateTextView.setText("registering");
+                mConnectionStateTextView.setText(R.string.connection_state_disconnected);
                 break;
             case XoClient.STATE_LOGIN:
-                this.setVisibility(View.VISIBLE);
-                mConnectionStateTextView.setText("logging in");
+            this.setVisibility(View.VISIBLE);
+                mConnectionStateTextView.setText(R.string.connection_state_login);
                 break;
             case XoClient.STATE_SYNCING:
                 this.setVisibility(View.VISIBLE);
-                mConnectionStateTextView.setText("syncing");
+                mConnectionStateTextView.setText(R.string.connection_state_synching);
                 break;
             case XoClient.STATE_READY:
+                this.setVisibility(View.GONE);
+                break;
+            case XoClient.STATE_REGISTERING:
                 this.setVisibility(View.GONE);
                 break;
         }
