1cbc708d6d5d306c3852e62a956becf6853dd94e
Mirko Geissler <mirko.geissler@artcom.de>
2015-03-10 10:21:12 +0100

    Use exception handling instead handling null references

--- android/src/main/java/com/hoccer/xo/android/content/selector/IContentCreator.java
+++ android/src/main/java/com/hoccer/xo/android/content/selector/IContentCreator.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import com.hoccer.talk.content.SelectedContent;
 
+import java.io.FileNotFoundException;
+
 public interface IContentCreator {
-    public SelectedContent apply(Context context, Intent intent);
+    public SelectedContent apply(Context context, Intent intent) throws Exception;
 }
