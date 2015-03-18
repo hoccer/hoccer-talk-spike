667aa91a2e87e890aa8ae33de8f8df3d6e698520
Jens Eraßmy <jens.erassmy@artcom.de>
2015-03-18 11:42:18 +0100

    Remove fragment tag

--- android/src/main/java/com/hoccer/xo/android/activity/PasswordPromptActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/PasswordPromptActivity.java
@@ -9,23 +9,20 @@ import android.view.inputmethod.InputMethodManager;
 import com.artcom.hoccer.R;
 import com.hoccer.xo.android.fragment.PasswordPromptFragment;
 
-public class PasswordPromptActivity extends FragmentActivity implements PasswordPromptFragment.OnPasswordProtectionUnlockListener{
+public class PasswordPromptActivity extends FragmentActivity implements PasswordPromptFragment.OnPasswordProtectionUnlockListener {
 
     public static final String EXTRA_ENABLE_BACK_NAVIGATION = "ENABLE_BACK";
-    private static final String PASSWORD_PROMPT_FRAGMENT = "PASSWORD_PROMPT_FRAGMENT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.default_framelayout);
-
         showPasswordPromptFragment();
     }
 
     private void showPasswordPromptFragment() {
-        Fragment fragment = new PasswordPromptFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fl_fragment_container, fragment, PASSWORD_PROMPT_FRAGMENT);
+        ft.replace(R.id.fl_fragment_container, new PasswordPromptFragment());
         ft.commit();
     }
 
@@ -39,7 +36,7 @@ public class PasswordPromptActivity extends FragmentActivity implements Password
 
     @Override
     public void onBackPressed() {
-        if (getIntent().getBooleanExtra(EXTRA_ENABLE_BACK_NAVIGATION, false)){
+        if (getIntent().getBooleanExtra(EXTRA_ENABLE_BACK_NAVIGATION, false)) {
             super.onBackPressed();
         }
     }
