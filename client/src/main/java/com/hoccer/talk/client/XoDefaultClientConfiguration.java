28f937371802721af669afff21f958b336cb0140
Mirko Geissler <mirko.geissler@artcom.de>
2015-03-18 12:39:53 +0100

    Reduce fixed backoff delay to 2 seconds

--- client/src/main/java/com/hoccer/talk/client/XoDefaultClientConfiguration.java
+++ client/src/main/java/com/hoccer/talk/client/XoDefaultClientConfiguration.java
@@ -68,7 +68,7 @@ public abstract class XoDefaultClientConfiguration implements IXoClientConfigura
 
     @Override
     public float getReconnectBackoffFixedDelay() {
-        return 3;
+        return 2;
     }
 
     @Override
