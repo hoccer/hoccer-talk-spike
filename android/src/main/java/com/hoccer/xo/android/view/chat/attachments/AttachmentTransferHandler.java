366f5e42c80b646f521813bea75b5c475f66ffb1
Jens Eraßmy <jens.erassmy@artcom.de>
2015-03-23 17:20:23 +0100

    Call method directly by TransferAgent

--- android/src/main/java/com/hoccer/xo/android/view/chat/attachments/AttachmentTransferHandler.java
+++ android/src/main/java/com/hoccer/xo/android/view/chat/attachments/AttachmentTransferHandler.java
@@ -57,7 +57,7 @@ public class AttachmentTransferHandler implements View.OnClickListener, IXoTrans
                             if (mTransfer instanceof TalkClientDownload) {
                                 LOG.debug("Will resume download for " + ((TalkClientDownload) mTransfer).getDownloadUrl());
                                 TalkClientDownload download = (TalkClientDownload) mTransfer;
-                                XoApplication.get().getXoClient().requestDownload(download, true);
+                                XoApplication.get().getXoClient().getTransferAgent().startOrRestartDownload(download, true);
                             }
                             break;
                         case CANCEL_DOWNLOAD:
