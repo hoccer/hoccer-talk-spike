218d5f23dbb60763a2a9fc3bef9daa46a47747da
Jens Eraßmy <jens.erassmy@artcom.de>
2015-05-12 15:20:20 +0200

    Set placeholder image source instead of background

--- android/src/main/java/com/hoccer/xo/android/view/Placeholder.java
+++ android/src/main/java/com/hoccer/xo/android/view/Placeholder.java
@@ -1,7 +1,5 @@
 package com.hoccer.xo.android.view;
 
-import android.content.res.Resources;
-import android.os.Build;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
@@ -26,18 +24,19 @@ public class Placeholder {
     public void applyToView(View view, View.OnClickListener onClickListener) {
         ImageView placeholderImage = (ImageView) view.findViewById(R.id.iv_contacts_placeholder);
         TextView placeholderText = (TextView) view.findViewById(R.id.tv_contacts_placeholder);
-        Resources resources = view.getResources();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            placeholderImage.setBackground(resources.getDrawable(mImageId));
-        } else {
-            placeholderImage.setBackgroundDrawable(resources.getDrawable(mImageId));
-        }
+        setupPlaceholderImage(onClickListener, placeholderImage);
+        setupPlaceholderText(view, placeholderText);
+    }
 
-        placeholderImage.setOnClickListener(onClickListener);
-
-        String text = resources.getString(mTextId);
+    private void setupPlaceholderText(View view, TextView placeholderText) {
+        String text = view.getResources().getString(mTextId);
         placeholderText.setMovementMethod(LinkMovementMethod.getInstance());
         placeholderText.setText(Html.fromHtml(text));
+    }
+
+    private void setupPlaceholderImage(View.OnClickListener onClickListener, ImageView placeholderImage) {
+        placeholderImage.setImageResource(mImageId);
+        placeholderImage.setOnClickListener(onClickListener);
     }
 }
