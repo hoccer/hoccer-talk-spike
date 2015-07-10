4cde58963667554ebdf5387fea47fdf4fdc89119
Jens Eraßmy <jens.erassmy@artcom.de>
2015-07-10 11:20:36 +0200

    Refactor ContactsAdapter

--- android/src/main/java/com/hoccer/xo/android/adapter/GroupManagementContactsAdapter.java
+++ android/src/main/java/com/hoccer/xo/android/adapter/GroupManagementContactsAdapter.java
@@ -1,7 +1,9 @@
 package com.hoccer.xo.android.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.CheckedTextView;
 import com.artcom.hoccer.R;
 import com.hoccer.talk.client.model.TalkClientContact;
@@ -42,22 +44,8 @@ public class GroupManagementContactsAdapter extends ContactsAdapter {
     }
 
     @Override
-    protected int getGroupLayout() {
-        return R.layout.item_contact_checked;
-    }
-
-    @Override
     protected int getSeparatorLayout() {
         return R.layout.item_contact_separator;
-    }
-
-    @Override
-    protected int getNearbyHistoryLayout() {
-        return -1;
-    }
-
-    @Override
-    protected void updateNearbyHistoryLayout(View v) {
     }
 
     @Override
@@ -65,10 +53,6 @@ public class GroupManagementContactsAdapter extends ContactsAdapter {
         LOG.debug("updateContact(" + contact.getClientContactId() + ")");
         CheckedTextView checkedTextView = (CheckedTextView) view.findViewById(R.id.contact_name_checked);
         checkedTextView.setText(contact.getNickname());
-
-        ViewGroup avatarContainer = (ViewGroup) view.findViewById(R.id.fl_avatar);
-        avatarContainer.removeAllViews();
-        avatarContainer.addView(AvatarView.inflate(contact, mActivity));
 
         try {
             TalkGroupMembership membership = mDatabase.findMembershipInGroupByClientId(mGroup.getGroupId(), contact.getClientId());
@@ -84,5 +68,4 @@ public class GroupManagementContactsAdapter extends ContactsAdapter {
             LOG.error(e);
         }
     }
-
 }
