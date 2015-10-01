86bfc90c44ca48859f644e95fa4964510484d6b8
Jens Eraßmy <jens.erassmy@artcom.de>
2015-10-01 11:10:43 +0200

    Clean up

--- android/src/main/java/com/hoccer/xo/android/activity/FaqTutorialActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/FaqTutorialActivity.java
@@ -2,7 +2,6 @@ package com.hoccer.xo.android.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.MailTo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
