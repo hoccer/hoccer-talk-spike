90f4400eff13bb34cdd002a69f36d1d84730bb5e
Jens Eraßmy <jens.erassmy@artcom.de>
2015-10-01 11:01:03 +0200

    Fix mailto url handling

--- android/src/main/java/com/hoccer/xo/android/activity/FaqTutorialActivity.java
+++ android/src/main/java/com/hoccer/xo/android/activity/FaqTutorialActivity.java
@@ -1,6 +1,9 @@
 package com.hoccer.xo.android.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.MailTo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,8 +17,23 @@ public class FaqTutorialActivity extends Activity {
         WebView webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
+
+            @Override
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 Toast.makeText(FaqTutorialActivity.this, description, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if(url.startsWith("mailto:")){
+                    Intent intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.setData(Uri.parse(url));
+                    intent.setType("text/plain");
+                    startActivity(intent);
+                    view.reload();
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
             }
         });
         webView.loadUrl(getIntent().getStringExtra("URL"));
