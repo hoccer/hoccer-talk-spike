c91e96da1609f18c722c45298a76f90817af2633
Jens Eraßmy <jens.erassmy@artcom.de>
2015-10-02 15:58:33 +0200

    Create correct selector if supported media was selected

--- android/src/main/java/com/hoccer/xo/android/dialog/FileSelector.java
+++ android/src/main/java/com/hoccer/xo/android/dialog/FileSelector.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.provider.MediaStore;
 import com.artcom.hoccer.R;
-import com.hoccer.talk.content.ContentMediaType;
 import com.hoccer.talk.content.SelectedContent;
 import com.hoccer.talk.content.SelectedFile;
-import com.hoccer.xo.android.content.selector.IContentSelector;
+import com.hoccer.xo.android.content.ContentUtils;
+import com.hoccer.xo.android.content.selector.*;
 import com.hoccer.xo.android.util.UriUtils;
 import com.hoccer.xo.android.util.colorscheme.ColoredDrawable;
+
+import static com.hoccer.talk.content.ContentMediaType.FILE;
 
 public class FileSelector implements IContentSelector {
 
@@ -45,6 +47,16 @@ public class FileSelector implements IContentSelector {
         String filePath = UriUtils.getFilePathByUri(context, intent.getData(), MediaStore.Images.Media.DATA);
         String mimeType = UriUtils.getMimeType(context, intent.getData());
 
-        return new SelectedFile(filePath, mimeType, ContentMediaType.DATA);
+        if (ContentUtils.isMimeTypeImage(mimeType)) {
+            return new ImageSelector(context).createObjectFromSelectionResult(context, intent);
+        } else if (ContentUtils.isMimeTypeVideo(mimeType)) {
+            return new VideoSelector(context).createObjectFromSelectionResult(context, intent);
+        } else if (ContentUtils.isMimeTypeAudio(mimeType)) {
+            return new AudioSelector(context).createObjectFromSelectionResult(context, intent);
+        } else if (ContentUtils.isMimeTypeContact(mimeType)) {
+            return new ContactSelector(context).createObjectFromSelectionResult(context, intent);
+        } else {
+            return new SelectedFile(filePath, mimeType, FILE);
+        }
     }
 }
