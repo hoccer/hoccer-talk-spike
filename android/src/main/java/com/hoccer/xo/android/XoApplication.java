d67c9b7d170fecf7c06fd847ff44f79820b4c338
Andreas Rettig <andreas.rettig@artcom.de>
2015-11-16 17:31:55 +0100

    Add polling start and stop

--- android/src/main/java/com/hoccer/xo/android/XoApplication.java
+++ android/src/main/java/com/hoccer/xo/android/XoApplication.java
@@ -12,6 +12,7 @@ import com.hoccer.talk.client.model.TalkClientDownload;
 import com.hoccer.talk.client.model.TalkClientUpload;
 import com.hoccer.talk.model.TalkPresence;
 import com.hoccer.xo.android.credentialtransfer.SrpChangeListener;
+import com.hoccer.xo.android.polling.PollingBroadcastReceiver;
 import com.hoccer.xo.android.service.XoClientService;
 import com.hoccer.xo.android.task.StartupTasks;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -200,6 +201,11 @@ public class XoApplication extends Application {
         sStartupTasks = new StartupTasks(this);
         sStartupTasks.executeRegisteredTasks();
 
+        if (sConfiguration.isPollingEnabled()) {
+            PollingBroadcastReceiver.startPolling(this);
+        } else {
+            PollingBroadcastReceiver.stopPolling(this);
+        }
     }
 
     private boolean isFirstConnectionAfterCrashOrUpdate() {
