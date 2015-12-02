1f3b65e7b46a4a07c6e3d2993321a84515aa1a8f
Andreas Rettig <andreas.rettig@artcom.de>
2015-12-02 17:35:16 +0100

    Extract constants for different disconnect times

--- client/src/main/java/com/hoccer/talk/client/XoClient.java
+++ client/src/main/java/com/hoccer/talk/client/XoClient.java
@@ -1096,7 +1096,7 @@ public class XoClient implements JsonRpcConnection.Listener, TransferListener {
                         if (mConnectInBackground) {
                             mConnectInBackground = false;
                             LOG.info("Trigger disconnect in 30 seconds..");
-                            disconnectAfterTimeout(30);
+                            disconnectAfterTimeout(mClientConfiguration.getBackgroundWakeDisconnectTimeoutSeconds());
                         }
                     }
                 });
@@ -1553,7 +1553,7 @@ public class XoClient implements JsonRpcConnection.Listener, TransferListener {
             public void run() {
                 if (isTransferInProgress()) {
                     LOG.debug("Transfer in progress. Postpone disconnect for 10 seconds.");
-                    disconnectAfterTimeout(10);
+                    disconnectAfterTimeout(mClientConfiguration.getPostponeDisconnectTimeoutSeconds());
                 } else {
                     mDisconnectTimeoutFuture = null;
                     switchState(State.DISCONNECTED, "disconnect timeout");
