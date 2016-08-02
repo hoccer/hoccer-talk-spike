9c6d694e9d676d617d4d2826571fc75d3143093b
Jens Eraßmy <jens.erassmy@artcom.de>
2016-08-02 16:38:40 +0200

    Use swipe to refresh to reload webpage

--- android/src/main/java/com/hoccer/xo/android/fragment/WebviewPagerFragment.java
+++ android/src/main/java/com/hoccer/xo/android/fragment/WebviewPagerFragment.java
@@ -3,6 +3,7 @@ package com.hoccer.xo.android.fragment;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import com.artcom.hoccer.R;
 import com.hoccer.xo.android.base.PagerFragment;
 import org.apache.log4j.Logger;
 
@@ -21,11 +23,19 @@ public class WebviewPagerFragment extends PagerFragment {
 
     @Override
     public void onPageSelected() {
-        webView.loadUrl(getArguments().getString("url"));
+
     }
 
     @Override
     public void onPageUnselected() {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        webView.loadUrl(getArguments().getString("url"));
     }
 
     @Override
@@ -49,16 +59,34 @@ public class WebviewPagerFragment extends PagerFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        webView = new WebView(getContext());
+        View view = inflater.inflate(R.layout.fragment_webview, container, false);
+
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
+
+        webView = (WebView) view.findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
             @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
                 super.onReceivedError(view, request, error);
                 LOG.error(error);
             }
         });
-        return webView;
+
+        return view;
     }
 
     @Override
